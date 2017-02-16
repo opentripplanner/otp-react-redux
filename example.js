6bc4147c52e575f8e9ed8037dbba4e2d4bcc5d5d
Landon Reed <landontreed@gmail.com>
2017-02-16 17:25:16 -0500

    initial react-router work

--- example.js
+++ example.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 
@@ -27,6 +29,8 @@ import {
   SwitchButton
 } from './lib'
 
+import OtpApp from './lib/components/otp-app'
+
 // load the OTP configuration
 import otpConfig from './config.yml'
 
@@ -47,50 +51,63 @@ import otpConfig from './config.yml'
 // set up the Redux store
 const store = createStore(
   combineReducers({
-    otp: createOtpReducer(otpConfig) // add optional initial query here
+    otp: createOtpReducer(otpConfig), // add optional initial query here
+    routing: routerReducer
     // add your own reducers if you want
   }),
   applyMiddleware(thunk, createLogger())
 )
 
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
 // define a simple responsive UI using Bootstrap and OTP-RR
 class OtpRRExample extends Component {
   render () {
-    return (<div className='otp'>
-      <Navbar fluid>
-        <Navbar.Brand>OpenTripPlanner</Navbar.Brand>
-      </Navbar>
-      <Grid fluid>
-        <Row>
-          <Col xs={12} md={4} className='sidebar'>
-            <LocationField type='from' label='Enter start location or click on map...' />
-            <LocationField type='to' label='Enter destination or click on map...' />
-            <SwitchButton />
-            <ModeSelector />
-            <DateTimeSelector />
-            <ErrorMessage />
-            <PlanTripButton />
-            {/* <NarrativeItineraries /> */}
-            <ItineraryCarousel />
-          </Col>
-
-          <Col xsHidden md={8} className='map-container'>
-            <BaseMap>
-              <BaseLayers />
-              <ItineraryOverlay />
-              <EndpointsOverlay />
-            </BaseMap>
-          </Col>
-        </Row>
-      </Grid>
-    </div>)
+    return (
+      <OtpApp>
+        <Navbar fluid>
+          <Navbar.Brand>OpenTripPlanner</Navbar.Brand>
+        </Navbar>
+        <Grid fluid>
+          <Row>
+            <Col xs={12} md={4} className='sidebar'>
+              <LocationField type='from' label='Enter start location or click on map...' />
+              <LocationField type='to' label='Enter destination or click on map...' />
+              <ModeSelector />
+              <DateTimeSelector />
+              <ErrorMessage />
+              <PlanTripButton />
+              <ItineraryCarousel />
+            </Col>
+            <Col xsHidden md={8} className='map-container'>
+              <BaseMap>
+                <BaseLayers />
+                <ItineraryOverlay />
+                <EndpointsOverlay />
+              </BaseMap>
+            </Col>
+          </Row>
+        </Grid>
+      </OtpApp>
+    )
   }
 }
 
 // render the app
 render(
   <Provider store={store}>
-    <OtpRRExample />
+    { /**
+      * If not using router history, simply include OtpRRExample here:
+      * e.g.
+      * <OtpRRExample />
+      */
+    }
+    <Router history={history}>
+      <Route path='/' component={props => <OtpApp {...props}><OtpRRExample /></OtpApp>} />
+      <Route path='/@:latLonZoom(/plan)' component={props => <OtpApp {...props}><OtpRRExample /></OtpApp>} />
+      <Route path='/plan' component={props => <OtpApp {...props}><OtpRRExample /></OtpApp>} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
