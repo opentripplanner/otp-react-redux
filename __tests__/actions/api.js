d124c3790f41c21a54ac3d52ffe5f7d4af9e8eff
Evan Siroky <evan.siroky@yahoo.com>
2017-03-06 19:37:21 -0800

    test(actions): add test for api actions

---
+++ __tests__/actions/api.js
@@ -0,0 +1,53 @@
+/* globals describe, expect, it, jest */
+
+import nock from 'nock'
+
+import {planTrip} from '../../lib/actions/api'
+
+function timeoutPromise (ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+const planTripAction = planTrip()
+
+describe('actions > api', () => {
+  it('should make a query to OTP', async () => {
+    nock('http://mock-host.com')
+      .get(/api\/plan/)
+      .reply(200, {
+        fake: 'response'
+      })
+
+    const mockDispatch = jest.fn()
+    planTripAction(mockDispatch, () => {
+      return {
+        otp: {
+          config: {
+            api: {
+              host: 'http://mock-host.com',
+              path: '/api',
+              port: 80
+            }
+          },
+          currentQuery: {
+            from: {
+              lat: 12,
+              lon: 34
+            },
+            to: {
+              lat: 34,
+              lon: 12
+            }
+          },
+          searches: []
+        }
+      }
+    })
+
+    await timeoutPromise(100)
+
+    expect(mockDispatch.mock.calls).toMatchSnapshot()
+  })
+})
