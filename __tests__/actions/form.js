0d6f14bdf9464c7b119c9fe149493d3c36a8e44b
Landon Reed <landontreed@gmail.com>
2019-06-26 14:55:17 -0400

    test(form): rename form.js (from form.js_old) and fix broken test

--- __tests__/actions/form.js
+++ __tests__/actions/form.js
@@ -1,14 +1,25 @@
-// Test file renamed because it is no longer valid
 // TODO: write tests for form actions
 
 /* globals describe, expect, it, jest */
 
-import {timeoutPromise} from '../test-utils'
+import { timeoutPromise } from '../test-utils'
 
-import {formChanged} from '../../lib/actions/form'
+import { formChanged } from '../../lib/actions/form'
 
 describe('actions > api > form', () => {
   it('should debounce numerous requests to plan a trip', async () => {
+    const queries = [
+      // Query 1
+      {
+        from: { lat: 12, lon: 34 },
+        to: { lat: 34, lon: 12 }
+      },
+      // Query 2 (from location changed)
+      {
+        from: { lat: 11, lon: 33 },
+        to: { lat: 34, lon: 12 }
+      }
+    ]
     const defaultState = {
       otp: {
         config: {
@@ -20,18 +31,15 @@ describe('actions > api > form', () => {
           autoPlan: true,
           debouncePlanTimeMs: 500
         },
-        currentQuery: {
-          from: { lat: 12, lon: 34 },
-          to: { lat: 34, lon: 12 }
-        },
+        currentQuery: queries[0],
         searches: []
       }
     }
 
-    const formChangedAction = formChanged()
-
     const mockDispatch = jest.fn()
     for (var i = 0; i < 10; i++) {
+      // Alternate back and forth between two queries on each iteration.
+      const formChangedAction = formChanged(queries[i % 2], queries[(i + 1) % 2])
       formChangedAction(mockDispatch, () => defaultState)
     }
 
