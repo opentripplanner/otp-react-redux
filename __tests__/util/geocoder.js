65da24a4add34f0465cc1ce88a40fe2a6fbc7a75
Evan Siroky <evan.siroky@yahoo.com>
2019-07-24 17:32:57 -0700

    test(geocoder): fix broken tests, and add one more test

--- __tests__/util/geocoder.js
+++ __tests__/util/geocoder.js
@@ -25,13 +25,17 @@ describe('geocoder', () => {
     .query(true)
     .replyWithFile(200, mockResponsePath('arcgis', 'suggest-response.json'))
     // reverse
-    .get(`${baseArcGisPath}findAddressCandidates`)
-    .query(true)
-    .replyWithFile(200, mockResponsePath('arcgis', 'findAddressCandidates-response.json'))
-    // search
     .get(`${baseArcGisPath}reverseGeocode`)
     .query(true)
     .replyWithFile(200, mockResponsePath('arcgis', 'reverseGeocode-response.json'))
+    // search
+    .get(`${baseArcGisPath}findAddressCandidates`)
+    .query(true)
+    .replyWithFile(200, mockResponsePath('arcgis', 'findAddressCandidates-response.json'))
+    // a 2nd search for purposes of resolving getLocationFromGeocodedFeature test
+    .get(`${baseArcGisPath}findAddressCandidates`)
+    .query(true)
+    .replyWithFile(200, mockResponsePath('arcgis', 'findAddressCandidates-response.json'))
 
   // nocks for PELIAS
   const basePeliasPath = '/pelias/v1/'
@@ -65,6 +69,36 @@ describe('geocoder', () => {
       it('should make reverse query', async () => {
         const result = await getGeocoder(geocoder)
           .reverse({ point: { lat: 45.516198, lon: -122.673240 } })
+        expect(result).toMatchSnapshot()
+      })
+
+      it('should get location from geocode feature', async () => {
+        let mockFeature
+        switch (geocoder.type) {
+          case 'ARCGIS':
+            mockFeature = {
+              magicKey: 'abcd',
+              properties: {
+                label: 'Mill Ends City Park, Portland, OR, USA'
+              },
+              text: 'Mill Ends City Park, Portland, OR, USA'
+            }
+            break
+          case 'PELIAS':
+            mockFeature = {
+              geometry: {
+                coordinates: [-122.673240, 45.516198],
+                type: 'Point'
+              },
+              properties: {
+                label: 'Mill Ends Park, Portland, OR, USA'
+              }
+            }
+            break
+          default:
+            throw new Error(`no mock feature defined for geocoder type: ${geocoder.type}`)
+        }
+        const result = await getGeocoder(geocoder).getLocationFromGeocodedFeature(mockFeature)
         expect(result).toMatchSnapshot()
       })
     })
