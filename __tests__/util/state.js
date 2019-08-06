f53201feba3646b768307750f4cd05adeb1b952d
Evan Siroky <evan.siroky@yahoo.com>
2019-08-06 12:26:16 -0700

    fix(time): make default query react properly to homeTimezone config

--- __tests__/util/state.js
+++ __tests__/util/state.js
@@ -1,13 +1,8 @@
 /* globals describe, expect, it */
 
-import {getDefaultQuery, queryIsValid} from '../../lib/util/state'
+import {queryIsValid} from '../../lib/util/state'
 
 describe('util > state', () => {
-  it('getDefaultQuery should parse window hash if available', () => {
-    window.location.hash = '#plan?arriveBy=false&date=2017-02-03&fromPlace=12,34&toPlace=34,12&time=12:34'
-    expect(getDefaultQuery()).toMatchSnapshot()
-  })
-
   describe('queryIsValid', () => {
     const fakeFromLocation = {
       lat: 12,
