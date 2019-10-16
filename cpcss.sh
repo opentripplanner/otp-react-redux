bec4c4b05f98b52aa3430e948b5e7207481fcc5a
Evan Siroky <evan.siroky@yahoo.com>
2019-10-16 16:07:53 -0700

    refactor: change directories for cp scripts

--- cpcss.sh
+++ cpcss.sh
@@ -1,5 +1,4 @@
 echo "building and copying css.."
 mastarm build
-cp ~/dev/otp/otp-react-redux/dist/index.css* ~/dev/otp/trimet-mod-otp/node_modules/otp-react-redux/dist/
+cp ~/git/otp-react-redux/dist/index.css* ~/git/trimet-mod-otp/node_modules/otp-react-redux/dist/
 echo "done"
-
