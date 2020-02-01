8fa6088c722f0f23b13e3697562077b7fdf265e1
Evan Siroky <evan.siroky@yahoo.com>
2020-01-31 19:14:12 -0800

    Add route sort comparator for route type

--- __tests__/util/itinerary.js
+++ __tests__/util/itinerary.js
@@ -1,71 +1,32 @@
 import {isTransit, makeRouteComparator} from '../../lib/util/itinerary'
 
+const {
+  route1,
+  route2,
+  route3,
+  route4,
+  route5,
+  route6,
+  route7,
+  route8,
+  route9,
+  route10,
+  route11,
+  route12,
+  route13
+} = require('./__mocks__/itinerary.json')
+
+function sortRoutes (...routes) {
+  routes.sort(makeRouteComparator())
+  return routes
+}
+
 describe('util > itinerary', () => {
   it('isTransit should work', () => {
     expect(isTransit('CAR')).toBeFalsy()
   })
 
   describe('routeComparator', () => {
-    const route1 = {
-      longName: 'Across town',
-      shortName: '10',
-      sortOrder: 10
-    }
-    const route2 = {
-      longName: 'Around town',
-      shortName: '20',
-      sortOrder: 2
-    }
-    const route3 = {
-      longName: 'Around another town',
-      shortName: '3',
-      sortOrder: -999
-    }
-    const route4 = {
-      longName: 'Loop route',
-      shortName: '2',
-      sortOrder: -999
-    }
-    const route5 = {
-      longName: 'A-line',
-      shortName: 'A',
-      sortOrder: -999
-    }
-    const route6 = {
-      longName: 'B-line',
-      shortName: 'B',
-      sortOrder: -999
-    }
-    const route7 = {
-      longName: 'A meandering route',
-      sortOrder: -999
-    }
-    const route8 = {
-      longName: 'Zig-zagging route',
-      shortName: '30',
-      sortOrder: 2
-    }
-    const route9 = {
-      longName: 'Express route',
-      shortName: '30',
-      sortOrder: 2
-    }
-    const route10 = {
-      longName: 'Variation of express route',
-      shortName: '30',
-      sortOrder: 2
-    }
-    const route11 = {
-      longName: 'Local route',
-      shortName: '6',
-      sortOrder: 2
-    }
-
-    function sortRoutes (...routes) {
-      routes.sort(makeRouteComparator())
-      return routes
-    }
-
     it('should sort routes based off of sortOrder', () => {
       expect(sortRoutes(route1, route2)).toMatchSnapshot()
     })
@@ -109,12 +70,21 @@ describe('util > itinerary', () => {
         route7,
         route8,
         route9,
-        route10
+        route10,
+        route11,
+        route12,
+        route13
       )).toMatchSnapshot()
     })
 
     it('should skip integer short name criteria when specified', () => {
       const routes = [route10, route11]
+      routes.sort(makeRouteComparator(false))
+      expect(routes).toMatchSnapshot()
+    })
+
+    it('should sort based off of route type', () => {
+      const routes = [route12, route13]
       routes.sort(makeRouteComparator(false))
       expect(routes).toMatchSnapshot()
     })
