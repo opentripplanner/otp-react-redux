bab3ef0da193cf927069e772c7f07b6f7b394369
Evan Siroky <evan.siroky@yahoo.com>
2020-11-17 14:26:22 -0800

    refactor: use react context for itinerary body and footer

--- example.js
+++ example.js
@@ -19,6 +19,7 @@ import {
   CallTakerControls,
   CallTakerPanel,
   CallTakerWindows,
+  DefaultItinerary,
   DefaultMainPanel,
   DesktopNav,
   BatchRoutingPanel,
@@ -49,6 +50,7 @@ if (useCustomIcons) {
 // define some application-wide components that should be used in
 // various places
 const components = {
+  ItineraryBody: DefaultItinerary,
   LegIcon: MyLegIcon,
   ModeIcon: MyModeIcon
 }
