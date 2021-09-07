b37c86ea7169cb3e52b08d81e51dcd5ced5b14cc
miles-grant-ibigroup <miles.grant@ibigroup.com>
2021-09-07 12:42:39 +0100

    refactor: fix pdfkit through webpack

--- craco.config.js
+++ craco.config.js
@@ -1,27 +1,22 @@
-const path = require("path");
+const path = require('path')
 
-const { addBeforeLoader, loaderByName } = require("@craco/craco");
-const BabelRcPlugin = require("@jackwilsdon/craco-use-babelrc");
+const { addBeforeLoader, loaderByName } = require('@craco/craco')
+const BabelRcPlugin = require('@jackwilsdon/craco-use-babelrc')
 
 module.exports = {
   plugins: [{ plugin: BabelRcPlugin }],
   webpack: {
-    configure: function(webpackConfig, { paths }) {
+    configure: function (webpackConfig, { paths }) {
       const yamlLoader = {
-        loader: ["json-loader", "yaml-loader"],
-        test: /\.(yml|yaml)$/,
-      };
-      paths.appBuild = webpackConfig.output.path = path.resolve("dist");
-      paths.appSrc = path.resolve(__dirname, "lib");
-      paths.appIndexJs = path.resolve(__dirname, "example.js");
-      webpackConfig.entry = paths.appIndexJs;
-      addBeforeLoader(webpackConfig, loaderByName("file-loader"), yamlLoader);
-      return webpackConfig;
-    },
-    externals: [
-      {
-        pdfkit: "commonjs2 pdfkit",
-      },
-    ],
-  },
-};
+        loader: ['json-loader', 'yaml-loader'],
+        test: /\.(yml|yaml)$/
+      }
+      paths.appBuild = webpackConfig.output.path = path.resolve('dist')
+      paths.appSrc = path.resolve(__dirname, 'lib')
+      paths.appIndexJs = path.resolve(__dirname, 'example.js')
+      webpackConfig.entry = paths.appIndexJs
+      addBeforeLoader(webpackConfig, loaderByName('file-loader'), yamlLoader)
+      return webpackConfig
+    }
+  }
+}
