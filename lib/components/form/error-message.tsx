605989fe0e74c66429ccee16b3883bb62eedb684
miles-grant-ibigroup <miles.grant@ibigroup.com>
2021-11-30 10:11:53 +0000

    refactor: add typescript/typescript TODOs

--- lib/components/form/error-message.tsx
+++ lib/components/form/error-message.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { FormattedMessage, useIntl } from 'react-intl'
 import { connect } from 'react-redux'
+import { FormattedMessage, useIntl } from 'react-intl'
+import React from 'react'
 
-import TripTools from '../narrative/trip-tools'
-import Icon from '../util/icon'
 import { getActiveError, getErrorMessage } from '../../util/state'
+import Icon from '../util/icon'
+import TripTools from '../narrative/trip-tools'
 
 /**
  * A component which renders an error message.
@@ -13,22 +13,32 @@ import { getActiveError, getErrorMessage } from '../../util/state'
  * @param warning A boolean indicating that the error isn't fatal. Setting this to
  * true will hide the trip re-plan buttons and change the header message.
  */
-const ErrorMessage = ({ error, message, warning }) => {
+const ErrorMessage = ({
+  error,
+  message,
+  warning
+}: {
+  // TODO: Error type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error: any
+  message?: string
+  warning?: boolean
+}) => {
   const intl = useIntl()
   message = message || getErrorMessage(error, intl)
   if (!message) return null
 
   return (
-    <div className='error-message'>
-      <div className='header'>
-        <Icon type='exclamation-circle' />
+    <div className="error-message">
+      <div className="header">
+        <Icon type="exclamation-circle" />
         {warning ? (
-          <FormattedMessage id='components.ErrorMessage.warning' />
+          <FormattedMessage id="components.ErrorMessage.warning" />
         ) : (
-          <FormattedMessage id='components.ErrorMessage.header' />
+          <FormattedMessage id="components.ErrorMessage.header" />
         )}
       </div>
-      <div className='message'>{message}</div>
+      <div className="message">{message}</div>
       {!warning && <TripTools buttonTypes={['START_OVER', 'REPORT_ISSUE']} />}
     </div>
   )
@@ -36,7 +46,7 @@ const ErrorMessage = ({ error, message, warning }) => {
 
 // connect to the redux store
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state: unknown) => {
   return {
     error: getActiveError(state)
   }
