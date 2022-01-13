3aa1cee089658307d9993dcaccef1ba556fc3c2c
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-01-13 14:00:58 +0100

    improvement(mapillary-frame): show fake loading screen to provide click feedback

--- lib/components/map/mapillary-frame.tsx
+++ lib/components/map/mapillary-frame.tsx
@@ -1,5 +1,8 @@
 import { Button } from 'react-bootstrap'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// eslint-disable-next-line sort-imports-es6-autofix/sort-imports-es6
+import Icon from '../util/icon'
 
 const MapillaryFrame = ({
   id,
@@ -8,12 +11,34 @@ const MapillaryFrame = ({
   id: string
   onClose?: () => void
 }): React.ReactElement => {
+  const [fakeLoad, setFakeLoad] = useState(false)
+  useEffect(() => {
+    // If the ID changed, show a "fake" loading screen to indicate to the user
+    // something is happening
+    setFakeLoad(true)
+    setTimeout(() => setFakeLoad(false), 750)
+  }, [id])
+
   return (
     <div className="leg-diagram" style={{ height: '50vh', zIndex: 999 }}>
+      <div
+        style={{
+          alignItems: 'center',
+          display: fakeLoad ? 'flex' : 'none',
+          height: '100%',
+          justifyContent: 'center'
+        }}
+      >
+        <Icon className="fa-spin" type="spinner" />
+      </div>
       <iframe
         frameBorder="0"
         src={`https://www.mapillary.com/embed?image_key=${id}&style=photo`}
-        style={{ height: '100%', width: '100%' }}
+        style={{
+          display: fakeLoad ? 'none' : 'block',
+          height: '100%',
+          width: '100%'
+        }}
         title="Imagery of the street"
       />
       <Button
