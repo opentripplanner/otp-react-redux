a3c957c14e811b258dafc91bea4f3e3ea6862bd9
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-06-15 16:14:30 -0400

    fix(TripViewer): Display stop times using homeTimezone.

--- lib/components/viewers/departure-time.tsx
+++ lib/components/viewers/departure-time.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import type { Time } from '../util/types'
 
 interface Props {
+  originDate?: Date
   realTime?: boolean
   stopTime: Time
 }
@@ -12,9 +13,14 @@ interface Props {
 /**
  * Displays a formatted departure time (expressed in the agency time zone) for the given stop time.
  */
-const DepartureTime = ({ realTime, stopTime }: Props): JSX.Element => {
+const DepartureTime = ({
+  originDate,
+  realTime,
+  stopTime
+}: Props): JSX.Element => {
+  // If an originDate is defined, use that, otherwise use stopTime.serviceDay.
   // stopTime.serviceDay already corresponds to midnight in the agency's home timezone so no extra conversion is needed.
-  const startOfDate = new Date(stopTime.serviceDay * 1000)
+  const startOfDate = originDate || new Date(stopTime.serviceDay * 1000)
   const departureTimestamp = addSeconds(
     startOfDate,
     realTime ? stopTime.realtimeDeparture : stopTime.scheduledDeparture
