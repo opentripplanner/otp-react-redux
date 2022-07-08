0f68abb5e83589e75e6504d4b117f20f37e77cd0
Daniel Heppner <daniel.heppner@ibigroup.com>
2022-07-08 01:00:58 -0700

    refactor(transitive): intl injection

--- lib/components/map/connected-transitive-overlay.tsx
+++ lib/components/map/connected-transitive-overlay.tsx
@@ -1,69 +1,18 @@
 import { Company, Itinerary, Leg } from '@opentripplanner/types'
 import { connect } from 'react-redux'
-import { useIntl } from 'react-intl'
+import { injectIntl, IntlShape, useIntl } from 'react-intl'
 import React from 'react'
-import TransitiveCanvasOverlay, {
-  itineraryToTransitive
-  // @ts-expect-error transitive-overlay is not typescripted
-} from '@opentripplanner/transitive-overlay'
+import TransitiveCanvasOverlay from '@opentripplanner/transitive-overlay'
 
 import { getTransitiveData } from '../../util/state'
 
-type ItineraryData = {
-  companies?: Company[]
-  disableFlexArc: boolean
-  getTransitiveRouteLabel: (leg: Leg) => string
-  hasItineraryResponse: boolean
-  hasResponse: boolean
-  itineraryToRender: Itinerary
-  otpResponse: unknown
-}
-
 type Props = {
-  itineraryData: ItineraryData
+  intl: IntlShape
   labeledModes: string[]
   styles: {
     labels: Record<string, unknown>
     segmentLabels: Record<string, unknown>
   }
-}
-
-const ConnectedTransitiveOverlay = (props: Props) => {
-  const { itineraryData, labeledModes, styles } = props
-  const intl = useIntl()
-  if (!itineraryData) return null
-  const {
-    companies,
-    disableFlexArc,
-    getTransitiveRouteLabel,
-    hasItineraryResponse,
-    hasResponse,
-    itineraryToRender,
-    otpResponse
-  } = itineraryData
-
-  let transitiveData
-  if (hasResponse) {
-    if (hasItineraryResponse) {
-      transitiveData = itineraryToRender
-        ? itineraryToTransitive(itineraryToRender, {
-            companies,
-            disableFlexArc,
-            getTransitiveRouteLabel,
-            intl
-          })
-        : null
-    } else if (otpResponse) {
-      transitiveData = otpResponse
-    }
-  }
-  return (
-    <TransitiveCanvasOverlay
-      labeledModes={labeledModes}
-      styles={styles}
-      transitiveData={transitiveData}
-    />
-  )
 }
 
 // connect to the redux store
@@ -80,15 +29,13 @@ const mapStateToProps = (state: Record<string, any>, ownProps: Props) => {
     return {}
   }
 
-  // @ts-expect-error state.js is not typescripted
-  const itineraryData: ItineraryData = getTransitiveData(state, ownProps)
-
   const obj = {
-    itineraryData,
     labeledModes,
-    styles
+    styles,
+    // @ts-expect-error state.js is not typescripted
+    transitiveData: getTransitiveData(state, ownProps)
   } // generate implicit type
   return obj
 }
 
-export default connect(mapStateToProps)(ConnectedTransitiveOverlay)
+export default injectIntl(connect(mapStateToProps)(TransitiveCanvasOverlay))
