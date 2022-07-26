6a9a45b4af3b90e1333520dffcc136f99e01a0ef
Daniel Heppner <daniel.heppner@ibigroup.com>
2022-07-26 14:40:06 -0700

    refactor(add-place-button): remove fa icons

---
+++ lib/components/form/add-place-button.tsx
@@ -0,0 +1,44 @@
+import { FormattedMessage } from 'react-intl'
+import { PlusCircle } from '@styled-icons/fa-solid/PlusCircle'
+import React from 'react'
+
+import StyledIconWrapper from '../util/styledIcon'
+
+type Props = {
+  from: unknown
+  intermediatePlaces: Array<unknown>
+  onClick: () => void
+  to: unknown
+}
+
+const AddPlaceButton = ({
+  from,
+  intermediatePlaces,
+  onClick,
+  to
+}: Props): JSX.Element => {
+  // Only permit adding intermediate place if from/to is defined.
+  const maxPlacesDefined = intermediatePlaces.length >= 3
+  const disabled = !from || !to || maxPlacesDefined
+  return (
+    <button
+      className="clear-button-formatting"
+      disabled={disabled}
+      onClick={onClick}
+      style={{ marginBottom: '5px', marginLeft: '10px' }}
+    >
+      <StyledIconWrapper spaceRight>
+        <PlusCircle />
+      </StyledIconWrapper>
+      {maxPlacesDefined ? (
+        <FormattedMessage id="components.AddPlaceButton.tooManyPlaces" />
+      ) : disabled ? (
+        <FormattedMessage id="components.AddPlaceButton.needOriginDestination" />
+      ) : (
+        <FormattedMessage id="components.AddPlaceButton.addPlace" />
+      )}
+    </button>
+  )
+}
+
+export default AddPlaceButton
