53f2836788c82117d2757f3de515aad272d8d8b4
Daniel Heppner <daniel.heppner@ibigroup.com>
2022-08-31 17:29:18 -0700

    refactor(narrative): consoldate util functions

--- lib/components/narrative/utils.tsx
+++ lib/components/narrative/utils.tsx
@@ -4,36 +4,10 @@ import React from 'react'
 import StyledIconWrapper from '../util/styledIcon'
 
 /**
- * Adds SVG icons to gradation map.
- * TODO: This function should be implemented where the icons are displayed,
- * rather than messily rewriting the gradation mapping object. Future accessilibty refactor?
- * @param SvgIcon SvgIcon from context
- * @param gradationMap Gradation mapping from config
- * @returns Gradation mapping with icons
- */
-export function addIconsGradationMap(
-  SvgIcon: React.ComponentType<{ iconName: string }>,
-  gradationMap: Record<string, any>
-): Record<string, any> {
-  const newGradationMap: Record<string, any> = {}
-  Object.keys(gradationMap).forEach((key) => {
-    newGradationMap[key] = gradationMap[key]
-    const iconName = gradationMap[key].icon
-    if (typeof iconName === 'string') {
-      newGradationMap[key].icon = (
-        <StyledIconWrapper>
-          <SvgIcon iconName={iconName} />
-        </StyledIconWrapper>
-      )
-    }
-  })
-
-  return newGradationMap
-}
-
-/**
  * Takes a gradation map and overrides the text with intl strings when available.
  * Also replaces icon string with actual icon components.
+ * TODO: This function should be implemented where the icons are displayed,
+ * rather than messily rewriting the gradation mapping object. Future accessilibty refactor?
  * @param intl React-Intl object
  * @param SvgIcon SvgIcon from context
  * @param gradationMap Gradation mapping from config
@@ -45,7 +19,8 @@ export function localizeGradationMap(
   gradationMap?: Record<string, any>
 ): Record<string, any> {
   if (!gradationMap) return {}
-  const newGradationMap = addIconsGradationMap(SvgIcon, gradationMap)
+  const newGradationMap: Record<string, any> = {}
+
   Object.keys(newGradationMap).forEach((key) => {
     // As these localization keys are in the config, rather than
     // standard language files, the message ids must be dynamically generated
@@ -54,6 +29,15 @@ export function localizeGradationMap(
     // Override the config label if a localized label exists
     if (localizationId !== localizedText) {
       newGradationMap[key].text = localizedText
+    }
+
+    const iconName = gradationMap[key].icon
+    if (typeof iconName === 'string') {
+      newGradationMap[key].icon = (
+        <StyledIconWrapper>
+          <SvgIcon iconName={iconName} />
+        </StyledIconWrapper>
+      )
     }
   })
   return newGradationMap
