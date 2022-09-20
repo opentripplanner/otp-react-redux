74b9c41dad38f8bff8bcea7bf4b6b83311e710cc
Daniel Heppner <daniel.heppner@ibigroup.com>
2022-09-19 17:56:55 -0700

    refactor(draggable-window): change to functional component

--- lib/components/admin/draggable-window.tsx
+++ lib/components/admin/draggable-window.tsx
@@ -1,6 +1,6 @@
 import { Times } from '@styled-icons/fa-solid'
 import Draggable, { DraggableProps } from 'react-draggable'
-import React, { Component, HTMLAttributes, ReactNode } from 'react'
+import React, { HTMLAttributes, ReactNode } from 'react'
 
 import StyledIconWrapper from '../util/styledIcon'
 
@@ -13,83 +13,80 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   scroll?: boolean
 }
 
-export default class DraggableWindow extends Component<Props> {
-  render(): ReactNode {
-    const {
-      children,
-      draggableProps,
-      footer,
-      header,
-      height = '245px',
-      onClickClose = () => null,
-      scroll = true,
-      style
-    } = this.props
-    const GREY_BORDER = '#777 1.3px solid'
-    const overwrittenDraggableProps = {
-      ...draggableProps,
-      cancel: 'input',
-      handle: '.handle'
-    }
-    return (
-      <Draggable
-        // Prevent dragging when clicking inside an input (e.g., search bar)
-        {...overwrittenDraggableProps}
+export default function DraggableWindow({
+  children,
+  draggableProps,
+  footer,
+  header,
+  height = '245px',
+  onClickClose = () => null,
+  scroll = true,
+  style
+}: Props): React.ReactElement {
+  const GREY_BORDER = '#777 1.3px solid'
+  const overwrittenDraggableProps = {
+    ...draggableProps,
+    cancel: 'input',
+    handle: '.handle'
+  }
+  return (
+    <Draggable
+      // Prevent dragging when clicking inside an input (e.g., search bar)
+      {...overwrittenDraggableProps}
+    >
+      <div
+        style={{
+          backgroundColor: 'white',
+          border: GREY_BORDER,
+          borderRadius: '15px',
+          boxShadow: '0 0 8px',
+          paddingBottom: '10px',
+          position: 'absolute',
+          width: '350px',
+          zIndex: 9999999,
+          ...style
+        }}
       >
         <div
+          className="handle"
           style={{
-            backgroundColor: 'white',
-            border: GREY_BORDER,
-            borderRadius: '15px',
-            boxShadow: '0 0 8px',
-            paddingBottom: '10px',
-            position: 'absolute',
-            width: '350px',
-            zIndex: 9999999,
-            ...style
+            borderBottom: GREY_BORDER,
+            cursor: 'move',
+            padding: '5px'
           }}
         >
+          <button
+            className="clear-button-formatting pull-right"
+            onClick={onClickClose}
+            style={{ margin: '4px 4px 0 0' }}
+          >
+            <StyledIconWrapper>
+              <Times />
+            </StyledIconWrapper>
+          </button>
+          {header}
+        </div>
+        <div
+          style={{
+            height,
+            margin: '0px 5px',
+            overflowY: scroll ? 'scroll' : 'visible'
+          }}
+        >
+          {children}
+        </div>
+        {footer && (
           <div
-            className="handle"
+            className="window-footer"
             style={{
-              borderBottom: GREY_BORDER,
-              cursor: 'move',
+              borderTop: GREY_BORDER,
               padding: '5px'
             }}
           >
-            <button
-              className="clear-button-formatting pull-right"
-              onClick={onClickClose}
-              style={{ margin: '4px 4px 0 0' }}
-            >
-              <StyledIconWrapper>
-                <Times />
-              </StyledIconWrapper>
-            </button>
-            {header}
+            {footer}
           </div>
-          <div
-            style={{
-              height,
-              margin: '0px 5px',
-              overflowY: scroll ? 'scroll' : 'visible'
-            }}
-          >
-            {children}
-          </div>
-          {footer && (
-            <div
-              className="window-footer"
-              style={{
-                borderTop: GREY_BORDER,
-                padding: '5px'
-              }}
-            >
-              {footer}
-            </div>
-          )}
-        </div>
-      </Draggable>
-    )
-  }
+        )}
+      </div>
+    </Draggable>
+  )
 }
