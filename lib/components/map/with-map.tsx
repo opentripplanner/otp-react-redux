01b8c8e9d68193a3f4044f5a0e7a148ba3b8cfd4
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-09-22 12:01:42 -0400

    improvement(withMap): Add HOC to access native map object.

---
+++ lib/components/map/with-map.tsx
@@ -0,0 +1,18 @@
+import { MapRef, useMap } from 'react-map-gl'
+import React, { Component, FC } from 'react'
+
+/**
+ * Higher-order component that passes a map prop to its children.
+ * Intended to wrap around class components that are direct or indirect children of <MapProvider>.
+ * Function components should use react-map-gl's useMap instead.
+ */
+export default function withMap<T>(
+  ClassComponent: Component<T>
+): FC<T & { map: MapRef }> {
+  const WrappedComponent = (props: T): JSX.Element => {
+    const mapRefs = useMap()
+    return <ClassComponent {...props} map={mapRefs.current} />
+  }
+
+  return WrappedComponent
+}
