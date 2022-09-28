4c80a5beb23800b846fbd3059a96032b1caf4efc
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-09-28 15:16:35 -0400

    feat(PointPopup): Add zoom-to-location button, improve CSS.

--- lib/components/map/point-popup.tsx
+++ lib/components/map/point-popup.tsx
@@ -1,16 +1,25 @@
 import { connect } from 'react-redux'
-import { Popup, useMap } from 'react-map-gl'
+import { Popup as MapGlPopup, MapRef, useMap } from 'react-map-gl'
+import { Search } from '@styled-icons/fa-solid/Search'
 import { useIntl, WrappedComponentProps } from 'react-intl'
 import FromToLocationPicker from '@opentripplanner/from-to-location-picker'
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import styled from 'styled-components'
 import type { Location } from '@opentripplanner/types'
 
 import * as mapActions from '../../actions/map'
+import { Icon } from '../util/styledIcon'
 import { renderCoordinates } from '../form/user-settings'
 
-const PopupContainer = styled.div`
-  width: 240px;
+/**
+ * Adds a box shadow and tweaks border radius to make popups easier to read.
+ */
+const Popup = styled(MapGlPopup)`
+  & > .maplibregl-popup-content,
+  & > .mapboxgl-popup-content {
+    border-radius: 6px;
+    box-shadow: 0 3px 14px rgb(0 0 0 / 40%);
+  }
 `
 
 const PopupTitle = styled.div`
@@ -18,16 +27,30 @@ const PopupTitle = styled.div`
   margin-bottom: 6px;
 `
 
+const ZoomButton = styled.button`
+  background: none;
+  border: none;
+  padding-top: 0;
+`
+
 type Props = {
   mapPopupLocation: Location
   onSetLocationFromPopup: () => void
   setMapPopupLocation: (arg: { location: Location | null }) => void
+  zoomToPlace: (
+    map: MapRef,
+    place: { lat: number; lon: number },
+    zoom?: number
+  ) => void
 } & WrappedComponentProps
+
+const DEFAULT_ZOOM = 15
 
 function MapPopup({
   mapPopupLocation,
   onSetLocationFromPopup,
-  setMapPopupLocation
+  setMapPopupLocation,
+  zoomToPlace
 }: Props): JSX.Element | null {
   const intl = useIntl()
   const { current: map } = useMap()
@@ -35,11 +58,22 @@ function MapPopup({
 
   // Zoom out if zoomed in very far
   useEffect(() => {
-    if (map && mapPopupLocation && currentZoom !== null && currentZoom > 15) {
-      map.setZoom(15)
+    if (
+      map &&
+      mapPopupLocation &&
+      currentZoom !== null &&
+      currentZoom > DEFAULT_ZOOM
+    ) {
+      map.setZoom(DEFAULT_ZOOM)
     }
     // Only check zoom if popup appears in a new place
   }, [mapPopupLocation, map, currentZoom])
+
+  // Memoize this callback because it shouldn't change (unlike the zoom click one).
+  const onPopupClose = useCallback(
+    () => setMapPopupLocation({ location: null }),
+    [setMapPopupLocation]
+  )
 
   if (!mapPopupLocation) return null
 
@@ -56,22 +90,27 @@ function MapPopup({
       closeOnClick
       latitude={mapPopupLocation.lat}
       longitude={mapPopupLocation.lon}
-      onClose={() => setMapPopupLocation({ location: null })}
+      onClose={onPopupClose}
     >
-      <PopupContainer>
-        <PopupTitle>
-          {typeof popupName === 'string' && popupName.split(',').length > 3
-            ? popupName.split(',').splice(0, 3).join(',')
-            : popupName}
-        </PopupTitle>
-        <div>
-          <FromToLocationPicker
-            label
-            location={{ ...mapPopupLocation, name: popupName }}
-            setLocation={onSetLocationFromPopup}
-          />
-        </div>
-      </PopupContainer>
+      <ZoomButton
+        className="pull-right"
+        onClick={() => zoomToPlace(map, mapPopupLocation, DEFAULT_ZOOM)}
+        title="Zoom to location"
+      >
+        <Icon Icon={Search} />
+      </ZoomButton>
+      <PopupTitle>
+        {typeof popupName === 'string' && popupName.split(',').length > 3
+          ? popupName.split(',').splice(0, 3).join(',')
+          : popupName}
+      </PopupTitle>
+      <div>
+        <FromToLocationPicker
+          label
+          location={{ ...mapPopupLocation, name: popupName }}
+          setLocation={onSetLocationFromPopup}
+        />
+      </div>
     </Popup>
   )
 }
@@ -86,7 +125,8 @@ const mapStateToProps = (state: any) => {
 
 const mapDispatchToProps = {
   setMapPopupLocation: mapActions.setMapPopupLocation,
-  setMapPopupLocationAndGeocode: mapActions.setMapPopupLocationAndGeocode
+  setMapPopupLocationAndGeocode: mapActions.setMapPopupLocationAndGeocode,
+  zoomToPlace: mapActions.zoomToPlace
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MapPopup)
