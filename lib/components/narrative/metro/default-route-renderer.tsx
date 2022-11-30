8c594f1291ae276389f151d06e94ea8cb11015ee
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-11-30 18:35:42 -0500

    fix(route-viewer): Improve route short name alignment and sizing.

--- lib/components/narrative/metro/default-route-renderer.tsx
+++ lib/components/narrative/metro/default-route-renderer.tsx
@@ -2,16 +2,13 @@ import { Leg } from '@opentripplanner/types'
 import React from 'react'
 import styled from 'styled-components'
 
-// TODO: This should not be a section, but using div/span
-// doesn't allow us to style it from within the RouteBlock
-const Block = styled.section<{ color: string }>`
+const Block = styled.span<{ color: string }>`
   background: #${(props) => props.color}1A;
-  padding: 3px 7px;
-  border-top: 5px solid #${(props) => props.color};
   border-radius: 5px;
-  text-align: center;
+  border-top: 5px solid #${(props) => props.color};
   display: inline-block;
-  max-width: 12vw;
+  max-width: 150px;
+  padding: 3px 7px;
 `
 
 type RouteRendererProps = {
