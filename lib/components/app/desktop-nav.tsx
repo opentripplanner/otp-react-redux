3be3651b7ca76c000c9bb0136afb3ac27e41fff1
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-01-19 13:58:21 -0500

    improvement(desktop-nav): Add banner container and improve header content.

--- lib/components/app/desktop-nav.tsx
+++ lib/components/app/desktop-nav.tsx
@@ -11,18 +11,7 @@ import NavLoginButtonAuth0 from '../user/nav-login-button-auth0'
 import AppMenu from './app-menu'
 import LocaleSelector from './locale-selector'
 import ViewSwitcher from './view-switcher'
-/**
- * The desktop navigation bar, featuring a `branding` logo or a `title` text
- * defined in config.yml, and a sign-in button/menu with account links.
- *
- * The `branding` and `title` parameters in config.yml are handled
- * and shown in this order in the navigation bar:
- * 1. If `branding` is defined, it is shown, and no title is displayed.
- * 2. If `branding` is not defined but if `title` is, then `title` is shown.
- * 3. If neither is defined, just show 'OpenTripPlanner' (DEFAULT_APP_TITLE).
- *
- * TODO: merge with the mobile navigation bar.
- */
+
 // Typscript TODO: otpConfig type
 export type Props = {
   otpConfig: any
@@ -30,67 +19,63 @@ export type Props = {
   setPopupContent: (url: string) => void
 }
 
+/**
+ * The desktop navigation bar, featuring a `branding` logo or a `title` text
+ * defined in config.yml, and a sign-in button/menu with account links.
+ *
+ * The `branding` and `title` parameters in config.yml are handled
+ * and shown in this order in the navigation bar:
+ * 1. If `branding` is defined, it is shown, and no title is displayed.
+ *    (The title is still rendered for screen readers and browsers that lack image support.)
+ * 2. If `branding` is not defined but if `title` is, then `title` is shown.
+ * 3. If neither is defined, just show 'OpenTripPlanner' (DEFAULT_APP_TITLE).
+ *
+ * TODO: merge with the mobile navigation bar.
+ */
 const DesktopNav = ({ otpConfig, popupTarget, setPopupContent }: Props) => {
   const { branding, persistence, title = DEFAULT_APP_TITLE } = otpConfig
   const { language: configLanguages } = otpConfig
   const showLogin = Boolean(getAuth0Config(persistence))
 
-  // Display branding and title in the order as described in the class summary.
-  let brandingOrTitle
-  if (branding) {
-    brandingOrTitle = (
-      <div
-        className={`icon-${branding}`}
-        // FIXME: Style hack for desktop view.
-        style={{ marginLeft: 50 }}
-      />
-    )
-  } else {
-    brandingOrTitle = (
-      <div className="navbar-title" style={{ marginLeft: 50 }}>
-        {title}
-      </div>
-    )
-  }
-
   return (
-    <Navbar fluid inverse>
-      {/* Required to allow the hamburger button to be clicked */}
-      <Navbar.Header role="banner" style={{ position: 'relative', zIndex: 2 }}>
-        <Navbar.Brand>
-          {/* TODO: Reconcile CSS class and inline style. */}
-          <div
-            className="app-menu-container"
-            style={{ color: 'white', float: 'left', marginTop: '5px' }}
-          >
+    <div role="banner">
+      <Navbar fluid inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
             <AppMenu />
-          </div>
+            <div
+              className={branding && `with-icon icon-${branding}`}
+              style={{ marginLeft: 50 }}
+            >
+              {/* A title is always rendered (e.g.for screen readers)
+                  but is visually-hidden if a branding icon is used. */}
+              <div className="navbar-title">{title}</div>
+            </div>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <ViewSwitcher sticky />
 
-          {brandingOrTitle}
-        </Navbar.Brand>
-      </Navbar.Header>
-      <ViewSwitcher sticky />
-
-      <Navbar.Collapse>
-        <Nav pullRight>
-          {popupTarget && (
-            <NavItem onClick={() => setPopupContent(popupTarget)}>
-              <FormattedMessage id={`config.popups.${popupTarget}`} />
-            </NavItem>
-          )}
-          {configLanguages &&
-            // Ensure that > 1 valid language is defined
-            Object.keys(configLanguages).filter(
-              (key) => key !== 'allLanguages' && configLanguages[key].name
-            ).length > 1 && (
-              <LocaleSelector configLanguages={configLanguages} />
+        <Navbar.Collapse>
+          <Nav pullRight>
+            {popupTarget && (
+              <NavItem onClick={() => setPopupContent(popupTarget)}>
+                <FormattedMessage id={`config.popups.${popupTarget}`} />
+              </NavItem>
             )}
-          {showLogin && (
-            <NavLoginButtonAuth0 id="login-control" links={accountLinks} />
-          )}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+            {configLanguages &&
+              // Ensure that > 1 valid language is defined
+              Object.keys(configLanguages).filter(
+                (key) => key !== 'allLanguages' && configLanguages[key].name
+              ).length > 1 && (
+                <LocaleSelector configLanguages={configLanguages} />
+              )}
+            {showLogin && (
+              <NavLoginButtonAuth0 id="login-control" links={accountLinks} />
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   )
 }
 
