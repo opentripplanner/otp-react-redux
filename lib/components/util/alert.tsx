7dca696bae78f2f8c3aca7113ea9a327397b3f30
Amy Corson <115499534+amy-corson-ibigroup@users.noreply.github.com>
2023-01-24 12:47:33 -0600

    Change alert util per suggestion
    Co-authored-by: binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>

--- lib/components/util/alert.tsx
+++ lib/components/util/alert.tsx
@@ -5,15 +5,13 @@ interface Props {
   children: React.ReactNode
 }
 
-export default function Alert({ children }: Props): ReactElement {
-  const TripDescription = styled.div`
-    background-color: rgba(19, 193, 193, 0.2);
-    border-radius: 5px;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.02);
-    font-size: 12px;
-    margin-bottom: 1em;
-    padding: 0.75em 1em;
-  `
+const Alert = styled.div`
+  background-color: rgba(19, 193, 193, 0.2);
+  border-radius: 5px;
+  box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.02);
+  font-size: 12px;
+  margin-bottom: 1em;
+  padding: 0.75em 1em;
+`
 
-  return <TripDescription>{children}</TripDescription>
-}
+export default Alert
