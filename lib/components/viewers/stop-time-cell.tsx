108352966d49cc164e91e3346f0496d84b6ef035
Adriana Cerić <62163307+AdrianaCeric@users.noreply.github.com>
2023-02-17 14:08:23 -0500

    Add includeSeconds parameter to more functions

--- lib/components/viewers/stop-time-cell.tsx
+++ lib/components/viewers/stop-time-cell.tsx
@@ -145,7 +145,10 @@ const StopTimeCell = ({
           isDue ? (
             <FormattedMessage id="components.StopTimeCell.imminentArrival" />
           ) : (
-            <FormattedDuration duration={secondsUntilDeparture} />
+            <FormattedDuration
+              duration={secondsUntilDeparture}
+              includeSeconds={false}
+            />
           )
         ) : (
           <DepartureTime realTime stopTime={stopTime} />
