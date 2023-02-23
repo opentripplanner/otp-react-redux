8f9ce2c733a9751d7dd2b1e7f32e2414a65f9d33
amy-corson-ibigroup <115499534+amy-corson-ibigroup@users.noreply.github.com>
2023-02-23 15:23:20 -0600

    update refresh stop time controls

--- lib/components/viewers/live-stop-times.tsx
+++ lib/components/viewers/live-stop-times.tsx
@@ -198,9 +198,6 @@ class LiveStopTimes extends Component<Props, State> {
             <FormattedMessage id="components.LiveStopTimes.autoRefresh" />
           </label>
           <button
-            // Functionality is provided by auto-refresh, this only adds confusion
-            // in a screen reader context
-            aria-hidden
             className="link-button pull-right percy-hide"
             onClick={this._refreshStopTimes}
             style={{ fontSize: 'small' }}
