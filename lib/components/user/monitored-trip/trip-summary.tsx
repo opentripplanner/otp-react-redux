a0cff939f9cf9f015825a307fca0a80a250be2b0
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-02-28 10:54:26 -0500

    refactor(TripSummary): Convert to TypeScript.

--- lib/components/user/monitored-trip/trip-summary.tsx
+++ lib/components/user/monitored-trip/trip-summary.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { FormattedMessage } from 'react-intl'
 import React, { useContext } from 'react'
 
@@ -6,12 +5,23 @@ import { ComponentContext } from '../../../util/contexts'
 import FormattedDuration from '../../util/formatted-duration'
 import SpanWithSpace from '../../util/span-with-space'
 
-const TripSummary = ({ monitoredTrip }) => {
+interface Props {
+  // TODO: use a more complete definition of monitored trip.
+  monitoredTrip: {
+    itinerary: {
+      duration: number
+      endTime: number
+      startTime: number
+    }
+  }
+}
+
+const TripSummary = ({ monitoredTrip }: Props): JSX.Element => {
   const { itinerary } = monitoredTrip
   const { duration, endTime, startTime } = itinerary
+  // @ts-expect-error No type on ComponentContext
   const context = useContext(ComponentContext)
-  const ModesAndRoutes = context.ItineraryBody
-  // TODO: use the modern itinerary summary built for trip comparison.
+  const ModesAndRoutes = context.ItineraryBody.ModesAndRoutes
   return (
     <div
       className="otp option default-itin"
