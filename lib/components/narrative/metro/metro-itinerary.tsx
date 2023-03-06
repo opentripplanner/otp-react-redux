6081693891e8512caa81ff2bf2decc313114be87
amy-corson-ibigroup <115499534+amy-corson-ibigroup@users.noreply.github.com>
2023-03-06 12:52:10 -0600

    revert: remove non-transit options keyboard a11y

--- lib/components/narrative/metro/metro-itinerary.tsx
+++ lib/components/narrative/metro/metro-itinerary.tsx
@@ -175,7 +175,7 @@ const ItineraryGrid = styled.div`
   }
 `
 
-const ItineraryGridSmall = styled.button`
+const ItineraryGridSmall = styled.div`
   border-radius: 10px;
   display: grid;
   gap: 0 2px;
@@ -502,7 +502,7 @@ class MetroItinerary extends NarrativeItinerary {
               </ItineraryGrid>
             )}
             {mini && (
-              <ItineraryGridSmall className="other-itin">
+              <ItineraryGridSmall>
                 <PrimaryInfo as="span">
                   <FormattedDuration
                     duration={itinerary.duration}
