0a64c1ef19d7001675b21e91f615da3b82fc6e9c
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-03-07 11:11:19 -0500

    refactor(PageTitle): Extract component to update page titles.

---
+++ lib/components/util/page-title.ts
@@ -0,0 +1,30 @@
+import { connect } from 'react-redux'
+import { useEffect } from 'react'
+
+interface Props {
+  appTitle: string
+  title?: string
+}
+
+/**
+ * Invisible component that changes the title to the specified text.
+ * (It will append the configured app title to the end.)
+ */
+const PageTitle = ({ appTitle, title }: Props): JSX.Element => {
+  useEffect(() => {
+    document.title = title + ' | ' + appTitle
+  }, [appTitle, title])
+
+  // Component renders nothing
+  return null
+}
+
+// connect to redux store
+
+const mapStateToProps = (state) => {
+  return {
+    appTitle: state.otp.config.title // or the default one
+  }
+}
+
+export default connect(mapStateToProps)(PageTitle)
