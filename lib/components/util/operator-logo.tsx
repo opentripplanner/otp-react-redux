6ab31a33325467e1fb91a578bd6b30f1550f45fa
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-03-09 11:26:50 -0500

    refactor(OperatorLogo): Remove useless alt and aria labels.

--- lib/components/util/operator-logo.tsx
+++ lib/components/util/operator-logo.tsx
@@ -1,5 +1,4 @@
 import { TransitOperator } from '@opentripplanner/types'
-import { useIntl } from 'react-intl'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -12,27 +11,8 @@ type Props = {
 }
 
 const OperatorLogo = ({ operator }: Props): JSX.Element | null => {
-  const intl = useIntl()
-
   if (!operator?.logo) return null
-
-  return (
-    <OperatorImg
-      alt={intl.formatMessage(
-        {
-          id: 'components.RouteRow.operatorLogoAltText'
-        },
-        { operatorName: operator.name }
-      )}
-      aria-label={intl.formatMessage(
-        {
-          id: 'components.RouteRow.operatorLogoAriaLabel'
-        },
-        { operatorName: operator.name }
-      )}
-      src={operator.logo}
-    />
-  )
+  return <OperatorImg alt={operator.name} src={operator.logo} />
 }
 
 export default OperatorLogo
