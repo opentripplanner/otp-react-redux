7df18d1bd9484713b59855ca0828834fab802a42
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-03-24 16:23:33 -0400

    refactor(RouteViewerOverlay): Reduce duplicate calculations.

--- lib/components/map/connected-route-viewer-overlay.ts
+++ lib/components/map/connected-route-viewer-overlay.ts
@@ -3,17 +3,24 @@ import memoize from 'lodash.memoize'
 import RouteViewerOverlay from '@opentripplanner/route-viewer-overlay'
 
 /**
+ * Helper function to extract the desired route object from the state.
+ */
+const extractRoute = (state: any) => {
+  const { routeId } = state.otp.ui.viewedRoute || {}
+  const { routes } = state.otp.transitIndex
+  return routeId && routes ? routes[routeId] : {}
+}
+
+/**
  * Helper to compute the displayed route data.
  */
 const getRouteData = (state: any) => {
-  const { patternId, routeId } = state.otp.ui.viewedRoute || {}
-  const { routes } = state.otp.transitIndex
-  const routeData = routeId && routes ? routes[routeId] : {}
+  const { patternId } = state.otp.ui.viewedRoute || {}
   // Discard the vehicles field. Vehicles are not needed for rendering the route shape,
   // and returning that field in mapStateToProps would cause unnecessary map bounds fitting,
   // especially when a user is zoomed in to an area.
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { patterns, vehicles, ...otherRouteData } = routeData
+  const { patterns, vehicles, ...otherRouteData } = extractRoute(state)
 
   const filteredPatterns =
     patternId && patterns
@@ -42,21 +49,17 @@ const getMemoizedRouteData = memoize(
 // connect to the redux store
 
 const mapStateToProps = (state: any) => {
-  const routeData = getRouteData(state)
-
+  const { patterns = {}, pending } = extractRoute(state)
   // If the state is not pending and filtered patterns contains at least one key,
   // memoize the route data (based on route id and pattern id)
   // to avoid unnecessary rerendering/refitting of the map.
   // (Memoizing has no purpose if the route or pattern info have not been fetched.)
   // Assumption: we don't expect route shapes to change during a browsing session.
-  // (Refresh the browser to get updated route shapes.)
-  const shouldMemoize =
-    !routeData.pending && Object.keys(routeData.patterns).length !== 0
-
+  const shouldMemoize = !pending && Object.keys(patterns).length !== 0
   return {
     clipToPatternStops:
       state.otp.config.routeViewer?.hideRouteShapesWithinFlexZones,
-    routeData: shouldMemoize ? getMemoizedRouteData(state) : routeData
+    routeData: shouldMemoize ? getMemoizedRouteData(state) : getRouteData(state)
   }
 }
 
