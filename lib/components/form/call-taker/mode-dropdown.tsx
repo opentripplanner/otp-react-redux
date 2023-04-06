99dc20e207dea8955477f462ab2c1842bfb2e701
Daniel Heppner <daniel.heppner@ibigroup.com>
2023-04-06 16:22:56 -0700

    refactor(calltaker): new mode dropdown

---
+++ lib/components/form/call-taker/mode-dropdown.tsx
@@ -0,0 +1,56 @@
+import { connect } from 'react-redux'
+
+import { getModuleConfig, Modules } from '../../../util/config'
+import { TransportMode } from '@opentripplanner/types'
+import React, { useState } from 'react'
+
+type DropdownOption = {
+  combination: TransportMode[]
+  label: string
+}
+
+type Props = {
+  modeDropdownOptions: DropdownOption[]
+  onChangeModes: (combination: TransportMode[]) => void
+}
+
+/**
+ * Dropdown selector for the Call Taker form that allows quick selection of the
+ * full set of exclusive modes and access modes + transit. This will also
+ * automatically apply any companies associated with the mode option to the
+ * query params (e.g., Uber for CAR_HAIL or one of the various bike/scooter
+ * rental companies).
+ */
+function ModeDropdown({ modeDropdownOptions, onChangeModes }: Props) {
+  const [selectedMode, setSelectedMode] = useState(modeDropdownOptions[0].label)
+
+  const onChange = React.useCallback(
+    (e: React.FocusEvent<HTMLSelectElement>) => {
+      setSelectedMode(e.target.value)
+      const newModes = modeDropdownOptions.find(
+        (mdo) => mdo.label === e.target.value
+      )?.combination
+      onChangeModes(newModes || [])
+    },
+    [modeDropdownOptions, onChangeModes]
+  )
+
+  return (
+    <select onBlur={onChange} onChange={onChange} value={selectedMode}>
+      {modeDropdownOptions?.map((o) => (
+        <option key={o.label} value={o.label}>
+          {o.label}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+const mapStateToProps = (state: any) => {
+  const moduleConfig = getModuleConfig(state, Modules.CALL_TAKER).options
+  return {
+    modeDropdownOptions: moduleConfig.modeDropdownOptions
+  }
+}
+
+export default connect(mapStateToProps)(ModeDropdown)
