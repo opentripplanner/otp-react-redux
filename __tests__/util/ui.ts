1f6c6a188651fa7efa954e718fe5f720d24cb171
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-05-25 18:26:51 -0400

    fix(narrative-itineraries): Display the active itinerary per URL param (desktop).

---
+++ __tests__/util/ui.ts
@@ -0,0 +1,27 @@
+import { getItineraryView, ItineraryView } from '../../lib/util/ui'
+
+describe('util > ui', () => {
+  describe('getItineraryView', () => {
+    it('returns a list mode by default or ui_activeItinerary is -1', () => {
+      expect(getItineraryView({})).toBe(ItineraryView.LIST)
+      expect(getItineraryView({ ui_activeItinerary: -1 })).toBe(
+        ItineraryView.LIST
+      )
+    })
+    it('returns a full itinerary view if URL contains ui_activeItinerary', () => {
+      expect(getItineraryView({ ui_activeItinerary: 2 })).toBe(
+        ItineraryView.FULL
+      )
+    })
+    it('returns the specified view mode when set URL', () => {
+      expect(getItineraryView({ ui_itineraryView: 'leg' })).toBe(
+        ItineraryView.LEG
+      )
+    })
+    it('returns the list mode when if URL contains ui_activeItinerary  specified view mode when set URL', () => {
+      expect(getItineraryView({ ui_itineraryView: 'leg' })).toBe(
+        ItineraryView.LEG
+      )
+    })
+  })
+})
