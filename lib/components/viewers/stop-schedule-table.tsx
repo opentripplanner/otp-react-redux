c6c6547852d99b836816e0a7d7b51d3e1e077987
miles-grant-ibigroup <miles.grant@ibigroup.com>
2023-06-01 15:41:33 -0400

    refactor: sort css

--- lib/components/viewers/stop-schedule-table.tsx
+++ lib/components/viewers/stop-schedule-table.tsx
@@ -21,11 +21,11 @@ const StyledTable = styled.table`
   height: 100%;
   width: 100%;
   th {
+    background-color: var(--main-base-color, white);
     box-shadow: 0 1px 0px 0px #ccc;
     font-size: 75%;
     position: sticky;
     top: 0px;
-    background-color: var(--main-base-color, white);
   }
   tr > * {
     border-bottom: 1px solid #ccc;
