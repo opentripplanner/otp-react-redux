8f7fdd2276b33ce9f36b24e69b4dc439694f9af0
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-06-01 11:45:43 -0400

    improvement(NewAccountWizard): Convert to func component, add page title. Rename new account msg id.

--- lib/components/user/new-account-wizard.tsx
+++ lib/components/user/new-account-wizard.tsx
@@ -1,6 +1,8 @@
-import { FormattedMessage } from 'react-intl'
+import { FormattedMessage, useIntl } from 'react-intl'
 import { FormikProps } from 'formik'
-import React, { Component } from 'react'
+import React, { useCallback } from 'react'
+
+import PageTitle from '../util/page-title'
 
 import { User } from './types'
 import AccountSetupFinishPane from './account-setup-finish-pane'
@@ -10,6 +12,10 @@ import SequentialPaneDisplay from './sequential-pane-display'
 import TermsOfUsePane from './terms-of-use-pane'
 import VerifyEmailPane from './verify-email-pane'
 
+// The props include Formik props that provide access to the current user data (stored in props.values)
+// and to its own blur/change/submit event handlers that automate the state.
+// We forward the props to each pane (via SequentialPaneDisplay) so that their individual controls
+// can be wired to be managed by Formik.
 type FormikUserProps = FormikProps<User>
 
 interface Props extends FormikUserProps {
@@ -20,75 +26,74 @@ interface Props extends FormikUserProps {
 /**
  * This component is the new account wizard.
  */
-class NewAccountWizard extends Component<Props> {
-  _handleCreateNewUser = (): void => {
-    const {
-      onCreate, // provided by UserAccountScreen
-      values: userData // provided by Formik
-    } = this.props
+const NewAccountWizard = ({
+  activePaneId,
+  onCreate, // provided by UserAccountScreen
+  ...formikProps // provided by Formik
+}: Props): JSX.Element => {
+  const { values: userData } = formikProps
+  const intl = useIntl()
 
+  const handleCreateNewUser = useCallback(() => {
     // Create a user record only if an id is not assigned.
     if (!userData.id) {
       onCreate(userData)
     }
+  }, [onCreate, userData])
+
+  if (activePaneId === 'verify') {
+    const verifyEmail = intl.formatMessage({
+      id: 'components.NewAccountWizard.verify'
+    })
+    return (
+      <>
+        <PageTitle title={verifyEmail} />
+        <h1>{verifyEmail}</h1>
+        <VerifyEmailPane {...formikProps} />
+      </>
+    )
   }
 
-  render(): JSX.Element {
-    // The props include Formik props that provide access to the current user data (stored in props.values)
-    // and to its own blur/change/submit event handlers that automate the state.
-    // We forward the props to each pane (via SequentialPaneDisplay) so that their individual controls
-    // can be wired to be managed by Formik.
-    const { activePaneId, onCreate, ...formikProps } = this.props
-    const { values: userData } = formikProps
-    if (activePaneId === 'verify') {
-      return (
-        <>
-          <h1>
-            <FormattedMessage id="components.NewAccountWizard.verify" />
-          </h1>
-          <VerifyEmailPane {...formikProps} />
-        </>
-      )
+  const { hasConsentedToTerms, notificationChannel = 'email' } = userData
+  const createNewAccount = intl.formatMessage({
+    id: 'components.NewAccountWizard.createNewAccount'
+  })
+  const paneSequence = [
+    {
+      disableNext: !hasConsentedToTerms,
+      id: 'terms',
+      onNext: handleCreateNewUser,
+      pane: TermsOfUsePane,
+      title: createNewAccount
+    },
+    {
+      disableNext: notificationChannel === 'sms' && !userData.phoneNumber,
+      id: 'notifications',
+      pane: NotificationPrefsPane,
+      title: <FormattedMessage id="components.NewAccountWizard.notifications" />
+    },
+    {
+      id: 'places',
+      pane: FavoritePlaceList,
+      title: <FormattedMessage id="components.NewAccountWizard.places" />
+    },
+    {
+      id: 'finish',
+      pane: AccountSetupFinishPane,
+      title: <FormattedMessage id="components.NewAccountWizard.finish" />
     }
+  ]
 
-    const { hasConsentedToTerms, notificationChannel = 'email' } = userData
-
-    const paneSequence = [
-      {
-        disableNext: !hasConsentedToTerms,
-        id: 'terms',
-        onNext: this._handleCreateNewUser,
-        pane: TermsOfUsePane,
-        title: <FormattedMessage id="components.NewAccountWizard.terms" />
-      },
-      {
-        disableNext: notificationChannel === 'sms' && !userData.phoneNumber,
-        id: 'notifications',
-        pane: NotificationPrefsPane,
-        title: (
-          <FormattedMessage id="components.NewAccountWizard.notifications" />
-        )
-      },
-      {
-        id: 'places',
-        pane: FavoritePlaceList,
-        title: <FormattedMessage id="components.NewAccountWizard.places" />
-      },
-      {
-        id: 'finish',
-        pane: AccountSetupFinishPane,
-        title: <FormattedMessage id="components.NewAccountWizard.finish" />
-      }
-    ]
-
-    return (
+  return (
+    <>
+      <PageTitle title={createNewAccount} />
       <SequentialPaneDisplay
         activePaneId={activePaneId}
         paneProps={formikProps}
         panes={paneSequence}
       />
-    )
-  }
+    </>
+  )
 }
 
 export default NewAccountWizard
