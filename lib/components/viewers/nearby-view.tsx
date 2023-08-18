a7f33752a4a14e5f1a4e5205f52632fdfa6dbc0a
Daniel Heppner <daniel.heppner@ibigroup.com>
2023-08-18 11:26:47 -0700

    start work on creating nearby view fetch function

--- lib/components/viewers/nearby-view.tsx
+++ lib/components/viewers/nearby-view.tsx
@@ -1,11 +1,25 @@
+import { connect } from 'react-redux'
 import React from 'react'
 
 type Props = {
   hideBackButton?: boolean
+  nearbyViewCoords?: { lat: number; lon: number }
 }
 
 function NearbyView(props: Props): JSX.Element {
-  return <>hello</>
+  return (
+    <>
+      hello {props.nearbyViewCoords?.lat} {props.nearbyViewCoords?.lon}
+    </>
+  )
 }
 
-export default NearbyView
+const mapStateToProps = (state: any) => {
+  const { ui } = state.otp
+  const { nearbyViewCoords } = ui
+  return {
+    nearbyViewCoords
+  }
+}
+
+export default connect(mapStateToProps)(NearbyView)
