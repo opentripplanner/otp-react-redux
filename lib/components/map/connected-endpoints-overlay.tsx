1cd733e41e58e3a57929ce9e3cd46b5452c54de9
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-09-07 11:07:34 -0400

    refactor(connected-endpoints-overlay): Convert to TypeScript.

--- lib/components/map/connected-endpoints-overlay.tsx
+++ lib/components/map/connected-endpoints-overlay.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable react/prop-types */
 import { connect } from 'react-redux'
-import { useIntl } from 'react-intl'
+import { IntlShape, useIntl } from 'react-intl'
+import { UserLocationAndType } from '@opentripplanner/types'
 import EndpointsOverlay from '@opentripplanner/endpoints-overlay'
-import React, { useCallback } from 'react'
+import React, { ComponentProps, useCallback } from 'react'
 
 import { clearLocation } from '../../actions/form'
 import { forgetPlace, rememberPlace } from '../../actions/user'
@@ -10,11 +10,16 @@ import { getActiveSearch, getShowUserSettings } from '../../util/state'
 import { getUserLocations } from '../../util/user'
 import { setLocation } from '../../actions/map'
 
+type Props = ComponentProps<typeof EndpointsOverlay> & {
+  forgetPlace: (place: string, intl: IntlShape) => void
+  rememberPlace: (arg: UserLocationAndType, intl: IntlShape) => void
+}
+
 const ConnectedEndpointsOverlay = ({
   forgetPlace,
   rememberPlace,
   ...otherProps
-}) => {
+}: Props): JSX.Element => {
   const intl = useIntl()
   const _forgetPlace = useCallback(
     (place) => {
@@ -39,8 +44,9 @@ const ConnectedEndpointsOverlay = ({
 }
 
 // connect to the redux store
+// TODO: Add TypeScript to this section.
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state: any) => {
   const { viewedRoute } = state.otp.ui
   // If the route viewer is active, do not show itinerary on map.
   // mainPanelContent is null whenever the trip planner is active.
@@ -53,13 +59,13 @@ const mapStateToProps = (state) => {
 
   // Use query from active search (if a search has been made) or default to
   // current query is no search is available.
-  const activeSearch = getActiveSearch(state)
+  const activeSearch: any = getActiveSearch(state)
   const query = activeSearch ? activeSearch.query : state.otp.currentQuery
   const showUserSettings = getShowUserSettings(state)
   const { from, to } = query
   // Intermediate places doesn't trigger a re-plan, so for now default to
   // current query. FIXME: Determine with TriMet if this is desired behavior.
-  const places = state.otp.currentQuery.intermediatePlaces.filter((p) => p)
+  const places = state.otp.currentQuery.intermediatePlaces.filter((p: any) => p)
 
   return {
     fromLocation: from,
