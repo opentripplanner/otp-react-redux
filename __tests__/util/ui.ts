05deb14b0f4d435bf499e79598724b96447d7304
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-10-04 17:32:17 -0400

    fix(util/ui): Display itins as list items of no itin active and url param is full.

--- __tests__/util/ui.ts
+++ __tests__/util/ui.ts
@@ -11,15 +11,29 @@ describe('util > ui', () => {
         ItineraryView.LIST
       )
     })
-    it('returns a full itinerary view if URL contains ui_activeItinerary', () => {
+    it('returns a full itinerary view if URL contains ui_activeItinerary that is not -1', () => {
       expect(getItineraryView({ ui_activeItinerary: 2 })).toBe(
         ItineraryView.FULL
       )
     })
-    it('returns the specified view mode when set in URL', () => {
-      expect(getItineraryView({ ui_itineraryView: 'leg' })).toBe(
-        ItineraryView.LEG
+    it('returns an itinerary list view if URL contains ui_activeItinerary=-1 regardless of ui_itineraryView', () => {
+      expect(getItineraryView({ ui_activeItinerary: -1 })).toBe(
+        ItineraryView.LIST
       )
+      expect(
+        getItineraryView({
+          ui_activeItinerary: -1,
+          ui_itineraryView: ItineraryView.FULL
+        })
+      ).toBe(ItineraryView.LIST)
+    })
+    it('returns the specified view mode when set in URL', () => {
+      expect(
+        getItineraryView({
+          ui_activeItinerary: 0,
+          ui_itineraryView: ItineraryView.LEG
+        })
+      ).toBe(ItineraryView.LEG)
     })
   })
 })
