348ec3b39506d13d5419ac72f9c52809d6f22397
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2023-11-21 15:29:16 -0500

    refactor(TripSummaryPane): Convert to TypeScript

--- lib/components/user/monitored-trip/trip-summary-pane.tsx
+++ lib/components/user/monitored-trip/trip-summary-pane.tsx
@@ -1,31 +1,36 @@
-import React from 'react'
 import { FormattedList, FormattedMessage } from 'react-intl'
+import React from 'react'
 
 import { dayFieldsToArray } from '../../../util/monitored-trip'
+import { MonitoredTrip } from '../types'
 import FormattedDayOfWeek from '../../util/formatted-day-of-week'
 import Strong from '../../util/strong-text'
 
 import TripSummary from './trip-summary'
 
+interface Props {
+  monitoredTrip: MonitoredTrip
+}
+
 /**
  * Displays the summary information of a monitored trip.
  */
-const TripSummaryPane = ({ monitoredTrip }) => {
+const TripSummaryPane = ({ monitoredTrip }: Props): JSX.Element => {
   const { itinerary, leadTimeInMinutes } = monitoredTrip
 
   if (!itinerary) {
     return (
       <div>
-        <FormattedMessage id='common.itineraryDescriptions.noItineraryToDisplay' />
+        <FormattedMessage id="common.itineraryDescriptions.noItineraryToDisplay" />
       </div>
     )
   } else {
     const days = (
       <FormattedList
-        type='conjunction'
-        value={dayFieldsToArray(monitoredTrip).map(
-          d => <FormattedDayOfWeek day={d} key={d} />
-        )}
+        type="conjunction"
+        value={dayFieldsToArray(monitoredTrip).map((d) => (
+          <FormattedDayOfWeek day={d} key={d} />
+        ))}
       />
     )
 
@@ -35,21 +40,22 @@ const TripSummaryPane = ({ monitoredTrip }) => {
         <TripSummary monitoredTrip={monitoredTrip} />
         <p>
           <FormattedMessage
-            id='components.TripSummaryPane.happensOnDays'
+            id="components.TripSummaryPane.happensOnDays"
             values={{ days, strong: Strong }}
           />
         </p>
         <p>
-          {monitoredTrip.isActive
-            ? <FormattedMessage
-              id={'components.TripSummaryPane.notifications'}
+          {monitoredTrip.isActive ? (
+            <FormattedMessage
+              id="components.TripSummaryPane.notifications"
               values={{ leadTimeInMinutes, strong: Strong }}
             />
-            : <FormattedMessage
-              id={'components.TripSummaryPane.notificationsDisabled'}
+          ) : (
+            <FormattedMessage
+              id="components.TripSummaryPane.notificationsDisabled"
               values={{ leadTimeInMinutes, strong: Strong }}
             />
-          }
+          )}
         </p>
       </>
     )
