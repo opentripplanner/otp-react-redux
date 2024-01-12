438299faae5fd1542ab13c9e5e221c0ebdd71746
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-01-12 17:41:02 -0500

    refactor(DeleteUser): Connect component to redux action

--- lib/components/user/delete-user.tsx
+++ lib/components/user/delete-user.tsx
@@ -1,12 +1,15 @@
+import { Auth0ContextInterface, useAuth0 } from '@auth0/auth0-react'
 import { Button } from 'react-bootstrap'
-import { FormattedMessage } from 'react-intl'
-import React from 'react'
+import { connect } from 'react-redux'
+import { FormattedMessage, IntlShape, useIntl } from 'react-intl'
+import React, { FormEvent, useCallback } from 'react'
 import styled from 'styled-components'
 
+import * as userActions from '../../actions/user'
 import { RED_ON_WHITE } from '../util/colors'
 
 interface Props {
-  onDelete: () => void
+  deleteUser: (auth0: Auth0ContextInterface, intl: IntlShape) => void
 }
 
 const DeleteButton = styled(Button)`
@@ -25,10 +28,36 @@ const DeleteButton = styled(Button)`
 /**
  * Renders a delete user button for the account settings page.
  */
-const DeleteUser = ({ onDelete }: Props): JSX.Element => (
-  <DeleteButton block bsSize="large" onClick={onDelete}>
-    <FormattedMessage id="components.DeleteUser.deleteMyAccount" />
-  </DeleteButton>
-)
+const DeleteUser = ({ deleteUser }: Props): JSX.Element => {
+  const auth0 = useAuth0()
+  const intl = useIntl()
 
-export default DeleteUser
+  const handleDelete = useCallback(
+    (evt: FormEvent) => {
+      // Avoid triggering onsubmit with formik (which would result in a save user call).
+      evt.preventDefault()
+      if (
+        window.confirm(
+          intl.formatMessage({
+            id: 'components.UserAccountScreen.confirmDelete'
+          })
+        )
+      ) {
+        deleteUser(auth0, intl)
+      }
+    },
+    [auth0, deleteUser, intl]
+  )
+
+  return (
+    <DeleteButton bsSize="large" onClick={handleDelete}>
+      <FormattedMessage id="components.DeleteUser.deleteMyAccount" />
+    </DeleteButton>
+  )
+}
+
+const mapDispatchToProps = {
+  deleteUser: userActions.deleteUser
+}
+
+export default connect(null, mapDispatchToProps)(DeleteUser)
