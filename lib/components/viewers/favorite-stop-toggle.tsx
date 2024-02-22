0e68ee8920707c9dfd980a51ae5d3ec2b09cd70d
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-02-22 16:54:27 -0500

    refactor(FavoriteStopToggle): Extract FavoriteStopToggle component.

---
+++ lib/components/viewers/favorite-stop-toggle.tsx
@@ -0,0 +1,86 @@
+import { Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { Star as StarRegular } from '@styled-icons/fa-regular/Star'
+import { Star as StarSolid } from '@styled-icons/fa-solid/Star'
+import React, { useCallback } from 'react'
+
+import * as userActions from '../../actions/user'
+import { AppReduxState } from '../../util/state-types'
+import { getPersistenceMode } from '../../util/user'
+import { getShowUserSettings } from '../../util/state'
+import { StyledIconWrapper } from '../util/styledIcon'
+
+type PatternStopTime = {
+  pattern: Pattern
+  stoptimes: StopTime[]
+}
+
+type StopData = Place & {
+  code: string
+  gtfsId: string
+  stoptimesForPatterns: PatternStopTime[]
+}
+
+interface Props {
+  enableFavoriteStops: boolean
+  forgetStop: (stopData: StopData) => void
+  isFavoriteStop: boolean
+  rememberStop: (stopData: StopData) => void
+  stopData?: StopData
+}
+
+/**
+ * Renders a clickable star to indicate/mark a stop as favorite.
+ */
+const FavoriteStopToggle = ({
+  enableFavoriteStops,
+  forgetStop,
+  isFavoriteStop,
+  rememberStop,
+  stopData
+}: Props): JSX.Element | null => {
+  const handleClick = useCallback(() => {
+    if (isFavoriteStop) forgetStop(stopData)
+    else rememberStop(stopData)
+  }, [forgetStop, isFavoriteStop, rememberStop, stopData])
+
+  return enableFavoriteStops ? (
+    <Button
+      bsSize="large"
+      bsStyle="link"
+      onClick={handleClick}
+      style={{
+        color: isFavoriteStop ? 'yellow' : 'black',
+        marginLeft: '5px',
+        padding: 0
+      }}
+    >
+      <StyledIconWrapper>
+        {isFavoriteStop ? <StarSolid /> : <StarRegular />}
+      </StyledIconWrapper>
+    </Button>
+  ) : null
+}
+
+// connect to redux store
+
+const mapStateToProps = (state: AppReduxState) => {
+  const showUserSettings = getShowUserSettings(state)
+  const { config, ui } = state.otp
+  const { persistence } = config
+  const { favoriteStops } = state.user.localUser
+  const stopId = ui.viewedStop.stopId
+
+  return {
+    enableFavoriteStops:
+      showUserSettings && getPersistenceMode(persistence).isLocalStorage,
+    isFavoriteStop: favoriteStops.findIndex((s) => s.id === stopId) !== -1
+  }
+}
+
+const mapDispatchToProps = {
+  forgetStop: userActions.forgetStop,
+  rememberStop: userActions.rememberStop
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FavoriteStopToggle)
