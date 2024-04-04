8a980f937f1838b803214ea73c0fd8136dcf7aba
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-04-04 17:45:15 -0400

    refactor(MainPanel): Extract MainPanel component

---
+++ lib/components/app/main-panel.tsx
@@ -0,0 +1,38 @@
+import { Route, Switch } from 'react-router'
+import React, { useContext } from 'react'
+
+import { ComponentContext } from '../../util/contexts'
+import NearbyView from '../viewers/nearby/nearby-view'
+import PatternViewer from '../viewers/pattern-viewer'
+import RouteViewer from '../viewers/route-viewer'
+import StopScheduleViewer from '../viewers/stop-schedule-viewer'
+import TripViewer from '../viewers/trip-viewer'
+
+const MainPanel = (): JSX.Element => {
+  // @ts-expect-error No type on ComponentContext
+  const { MainPanel: ConfigMainPanel } = useContext(ComponentContext)
+  return (
+    <Switch>
+      <Route exact path={['/nearby', '/nearby/:latLon']}>
+        <NearbyView hideBackButton />
+      </Route>
+      <Route exact path={['/schedule', '/schedule/:id']}>
+        <StopScheduleViewer hideBackButton />
+      </Route>
+      <Route exact path="/route/:id/pattern/:patternId">
+        <PatternViewer hideBackButton />
+      </Route>
+      <Route exact path={['/route', '/route/:id']}>
+        <RouteViewer hideBackButton />
+      </Route>
+      <Route exact path="/trip/:id">
+        <TripViewer hideBackButton />
+      </Route>
+      <Route exact path="/">
+        <ConfigMainPanel />
+      </Route>
+    </Switch>
+  )
+}
+
+export default MainPanel
