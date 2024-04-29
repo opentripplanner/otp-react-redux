eb3f10740415365d16e8c561e0747135a54ed075
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-04-29 19:10:08 -0400

    refactor(UserAccountScreen): Replace if-based render with router Switch/Route.

--- lib/components/user/user-account-screen.tsx
+++ lib/components/user/user-account-screen.tsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 import { Form, Formik, FormikProps } from 'formik'
 import { injectIntl, IntlShape } from 'react-intl'
-import { RouteComponentProps } from 'react-router'
+import { Route, RouteComponentProps, Switch } from 'react-router'
 import { withAuthenticationRequired } from '@auth0/auth0-react'
 import clone from 'clone'
 import React, { ChangeEvent, Component } from 'react'
@@ -11,7 +11,11 @@ import toast from 'react-hot-toast'
 import * as userActions from '../../actions/user'
 import { AppReduxState } from '../../util/state-types'
 import { cleanupMobilityDevices } from '../../util/user'
-import { CREATE_ACCOUNT_PATH, MOBILITY_PATH } from '../../util/constants'
+import {
+  CREATE_ACCOUNT_PATH,
+  CREATE_ACCOUNT_VERIFY_PATH,
+  MOBILITY_PATH
+} from '../../util/constants'
 import { RETURN_TO_CURRENT_ROUTE } from '../../util/ui'
 import { toastSuccess } from '../util/toasts'
 import PageTitle from '../util/page-title'
@@ -25,7 +29,6 @@ import VerifyEmailPane from './verify-email-pane'
 import withLoggedInUserSupport from './with-logged-in-user-support'
 
 interface Props {
-  basePath?: string
   createOrUpdateUser: (user: User, intl: IntlShape) => Promise<number>
   intl: IntlShape
   isWizard: boolean
@@ -172,7 +175,7 @@ class UserAccountScreen extends Component<Props> {
   }
 
   render() {
-    const { basePath, intl, isWizard, itemId, loggedInUser } = this.props
+    const { intl, isWizard, itemId, loggedInUser } = this.props
     const loggedInUserWithNotificationArray = {
       ...loggedInUser,
       notificationChannel: loggedInUser.notificationChannel?.split(',') || []
@@ -193,45 +196,42 @@ class UserAccountScreen extends Component<Props> {
               // We pass the Formik props below to the components rendered so that individual controls
               // can be wired to be managed by Formik.
               (formikProps) => {
-                if (itemId === 'verify') {
-                  const verifyEmail = intl.formatMessage({
-                    id: 'components.NewAccountWizard.verify'
-                  })
-                  return (
-                    <Form id="user-settings-form" noValidate>
-                      <PageTitle title={verifyEmail} />
-                      <h1>{verifyEmail}</h1>
-                      <VerifyEmailPane />
-                    </Form>
-                  )
-                }
-
                 const newFormikProps = {
                   ...formikProps,
                   // Use our own handleChange handler that wraps around Formik's.
                   handleChange: this._handleInputChange(formikProps)
                 }
+                const verifyEmail = intl.formatMessage({
+                  id: 'components.NewAccountWizard.verify'
+                })
 
-                if (basePath === CREATE_ACCOUNT_PATH) {
-                  return (
-                    <NewAccountWizard
-                      activePaneId={itemId}
-                      formikProps={newFormikProps}
-                      onCreate={this._handleCreateNewUser}
-                    />
-                  )
-                }
-
-                if (basePath === MOBILITY_PATH) {
-                  return (
-                    <MobilityWizard
-                      activePaneId={itemId}
-                      formikProps={newFormikProps}
-                    />
-                  )
-                }
-
-                return <ExistingAccountDisplay {...newFormikProps} />
+                return (
+                  <Switch>
+                    <Route exact path={CREATE_ACCOUNT_VERIFY_PATH}>
+                      <Form id="user-settings-form" noValidate>
+                        <PageTitle title={verifyEmail} />
+                        <h1>{verifyEmail}</h1>
+                        <VerifyEmailPane />
+                      </Form>
+                    </Route>
+                    <Route path={CREATE_ACCOUNT_PATH}>
+                      <NewAccountWizard
+                        activePaneId={itemId}
+                        formikProps={newFormikProps}
+                        onCreate={this._handleCreateNewUser}
+                      />
+                    </Route>
+                    <Route path={MOBILITY_PATH}>
+                      <MobilityWizard
+                        activePaneId={itemId}
+                        formikProps={newFormikProps}
+                      />
+                    </Route>
+                    <Route>
+                      <ExistingAccountDisplay {...newFormikProps} />
+                    </Route>
+                  </Switch>
+                )
               }
             }
           </Formik>
@@ -254,7 +254,6 @@ const mapStateToProps = (
 
   const { step } = params
   return {
-    basePath,
     isWizard: !!basePath,
     itemId: step,
     loggedInUser: state.user.loggedInUser
