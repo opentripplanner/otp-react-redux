3c990f18093364dbe8d2da5c86b939e681b20a1b
amy-corson-ibigroup <115499534+amy-corson-ibigroup@users.noreply.github.com>
2024-05-02 14:40:26 -0500

    feat(colors): Add building blocks and update color variables

--- lib/components/util/colors.ts
+++ lib/components/util/colors.ts
@@ -1,11 +1,23 @@
+import colors from '@opentripplanner/building-blocks'
+
+const { blue, grey, red } = colors
+
 /**
  * A red color similar to Bootstrap's "alert" background #d9534f,
  * but with enough contrast over white backgrounds
  */
-export const RED_ON_WHITE = '#d1332e'
+const RED_ON_WHITE = red[800]
 
 /**
  * A gray color similar to #808080 or #888,
  * but with enough contrast on white backgrounds.
  */
-export const GRAY_ON_WHITE = '#757575'
+const GRAY_ON_WHITE = grey[700]
+
+/**
+ * A blue for links and blue text on white
+ */
+
+const BLUE_LINKS = blue[800]
+
+export { grey, red, blue, RED_ON_WHITE, GRAY_ON_WHITE, BLUE_LINKS }
