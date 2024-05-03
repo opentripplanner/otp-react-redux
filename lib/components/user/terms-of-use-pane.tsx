095c952072dbea7c29fa0cff878516448cc04c0b
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-05-03 17:59:06 -0400

    feat(TermsOfUsePane): Hide terms of storage checkbox if configured.

--- lib/components/user/terms-of-use-pane.tsx
+++ lib/components/user/terms-of-use-pane.tsx
@@ -4,6 +4,7 @@ import { FormattedMessage, useIntl } from 'react-intl'
 import React, { FormEventHandler } from 'react'
 
 import { AppReduxState } from '../../util/state-types'
+import { CheckboxDefaultState } from '../../util/config-types'
 import { LinkOpensNewWindow } from '../util/externalLink'
 import {
   TERMS_OF_SERVICE_PATH,
@@ -19,6 +20,7 @@ const TermsOfUsePane = ({
   handleChange,
   locale,
   termsOfServiceLink,
+  termsOfStorageDefault,
   values: userData
 }: {
   disableCheckTerms: boolean
@@ -26,6 +28,7 @@ const TermsOfUsePane = ({
   handleChange: FormEventHandler<Checkbox>
   locale: string
   termsOfServiceLink?: string
+  termsOfStorageDefault?: CheckboxDefaultState
   values: {
     hasConsentedToTerms: boolean
     storeTripHistory: boolean
@@ -37,6 +40,8 @@ const TermsOfUsePane = ({
   const TOSLinkWithI18n = termsOfServiceLink?.replace('{locale}', locale)
 
   const termsURL = TOSLinkWithI18n || `/#${TERMS_OF_SERVICE_PATH}`
+
+  const isStorageVisible = termsOfStorageDefault.startsWith('visible-')
 
   return (
     <div>
@@ -61,51 +66,54 @@ const TermsOfUsePane = ({
           />
         </Checkbox>
       </FormGroup>
-      <FormGroup>
-        <Checkbox
-          checked={storeTripHistory}
-          name="storeTripHistory"
-          onBlur={handleBlur}
-          onChange={(e) => {
-            // Show alert when user is unchecking the checkbox
-            if (storeTripHistory) {
-              // Do nothing if the user hits cancel
-              if (
-                // eslint-disable-next-line no-restricted-globals
-                !confirm(
-                  intl.formatMessage({
-                    id: 'components.TermsOfUsePane.confirmDeletionPrompt'
-                  })
-                )
-              ) {
-                return
+      {isStorageVisible ? (
+        <FormGroup>
+          <Checkbox
+            checked={storeTripHistory}
+            name="storeTripHistory"
+            onBlur={handleBlur}
+            onChange={(e) => {
+              // Show alert when user is unchecking the checkbox
+              if (storeTripHistory) {
+                // Do nothing if the user hits cancel
+                if (
+                  // eslint-disable-next-line no-restricted-globals
+                  !confirm(
+                    intl.formatMessage({
+                      id: 'components.TermsOfUsePane.confirmDeletionPrompt'
+                    })
+                  )
+                ) {
+                  return
+                }
               }
-            }
 
-            handleChange(e)
-          }}
-        >
-          <FormattedMessage
-            id="components.TermsOfUsePane.termsOfStorageStatement"
-            values={{
-              termsOfStorageLink: (contents: JSX.Element) => (
-                <LinkOpensNewWindow
-                  contents={contents}
-                  inline
-                  url={`/#${TERMS_OF_STORAGE_PATH}`}
-                />
-              )
+              handleChange(e)
             }}
-          />
-        </Checkbox>
-      </FormGroup>
+          >
+            <FormattedMessage
+              id="components.TermsOfUsePane.termsOfStorageStatement"
+              values={{
+                termsOfStorageLink: (contents: JSX.Element) => (
+                  <LinkOpensNewWindow
+                    contents={contents}
+                    inline
+                    url={`/#${TERMS_OF_STORAGE_PATH}`}
+                  />
+                )
+              }}
+            />
+          </Checkbox>
+        </FormGroup>
+      ) : null}
     </div>
   )
 }
 const mapStateToProps = (state: AppReduxState) => {
   return {
     locale: state.otp.ui?.locale,
-    termsOfServiceLink: state.otp.config.termsOfServiceLink
+    termsOfServiceLink: state.otp.config.termsOfServiceLink,
+    termsOfStorageDefault: state.otp.config.termsOfStorageDefault
   }
 }
 
