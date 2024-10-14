44b8982e9098d7e0fd8f7b21cb4b022f92fc785d
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-10-14 14:54:31 -0400

    test(a11y): Add chromium user-directory arg

--- a11y/a11y.test.js
+++ a11y/a11y.test.js
@@ -58,7 +58,7 @@ beforeAll(async () => {
   })
   // Web security is disabled to allow requests to the mock OTP server
   browser = await puppeteer.launch({
-    args: ['--disable-web-security']
+    args: ['--disable-web-security', '--user-data-dir=/tmp/chromium-a11y']
   })
   console.log('Headless browser created.')
 })
