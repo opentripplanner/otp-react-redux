db7bb9e7dfc008b4b895865d0702cc34f3c7b3b9
danielhep <dh@danielheppner.com>
2025-02-10 17:28:57 -0800

    move error message check to bottom

--- percy/percy.test.js
+++ percy/percy.test.js
@@ -108,23 +108,12 @@ async function executeTest(page, isMobile, isCallTaker) {
   // Make sure that the main UI (incl. map controls) has loaded.
   await page.waitForSelector('.maplibregl-ctrl-zoom-in')
 
-  // Go to a URL that will trigger an error message.
-  await page.goto(
-    `http://localhost:${MOCK_SERVER_PORT}/#/?ui_activeSearch=fg33svlbf&ui_activeItinerary=-1&fromPlace=TestLocation%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A34.78946214120528%2C-86.37663414886111&toPlace=1%20TestLocation2%20Avenue%20NE%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A35.767060728439574%2C-86.35749390533111&date=2023-08-09&time=17%3A56&arriveBy=false&mode=BICYCLE&walkSpeed=1.34&numItineraries=3&modeButtons=car_transit`
-  )
-  // FIXME: Network idle condition seems never met after navigating to above link.
-  // await page.waitForNavigation({ waitUntil: 'networkidle2' })
-  await page.waitForTimeout(4000)
-  await page.waitForSelector('.options')
-  await percySnapshotWithWait(page, 'With error message displayed')
   // Load itinerary from URL
   // Triggers mock.har graphql query #1 and #2 (bike-only query, twice).
   // FIXME: Opening a url with non-default mode params triggers the plan query twice.
   await page.goto(
     `http://localhost:${MOCK_SERVER_PORT}/#/?ui_activeSearch=fg33svlbf&ui_activeItinerary=-1&fromPlace=South%20Prado%20Northeast%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A33.78946214120528%2C-84.37663414886111&toPlace=1%20Copenhill%20Avenue%20NE%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A33.767060728439574%2C-84.35749390533111&date=2023-08-09&time=17%3A56&arriveBy=false&mode=BICYCLE&walkSpeed=1.34&numItineraries=3&modeButtons=walk_bike`
   )
-  await openEditIfNeeded(page, isMobile)
-  await page.click('#plan-trip')
   // FIXME: Network idle condition seems never met after navigating to above link.
   // await page.waitForNavigation({ waitUntil: 'networkidle2' })
   await page.waitForTimeout(4000)
@@ -389,6 +378,19 @@ async function executeTest(page, isMobile, isCallTaker) {
   if (isMobile) {
     // Printable itinerary screenshot on mobile only better page ration (and to save allowance).
     await percySnapshotWithWait(page, 'Printable Itinerary')
+  }
+  if (!isCallTaker) {
+    // Go to a URL that will trigger an error message.
+    await page.goto(
+      `http://localhost:${MOCK_SERVER_PORT}/#/?ui_activeSearch=fg33svlbf&ui_activeItinerary=-1&fromPlace=TestLocation%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A34.78946214120528%2C-86.37663414886111&toPlace=1%20TestLocation2%20Avenue%20NE%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A35.767060728439574%2C-86.35749390533111&date=2023-08-09&time=17%3A56&arriveBy=false&mode=BICYCLE&walkSpeed=1.34&numItineraries=3&modeButtons=car_transit`
+    )
+    await openEditIfNeeded(page, isMobile)
+    await page.click('#plan-trip')
+    // FIXME: Network idle condition seems never met after navigating to above link.
+    // await page.waitForNavigation({ waitUntil: 'networkidle2' })
+    await page.waitForTimeout(2000)
+    await page.waitForSelector('.options')
+    await percySnapshotWithWait(page, 'With error message displayed')
   }
 }
 
