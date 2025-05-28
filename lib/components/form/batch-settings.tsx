8de4c26713b1be08eabb73a14fa6adac36605af4
Amy Corson <115499534+amy-corson-ibigroup@users.noreply.github.com>
2025-05-28 15:14:23 -0500

    add type: submit to plan trip button

--- lib/components/form/batch-settings.tsx
+++ lib/components/form/batch-settings.tsx
@@ -105,6 +105,7 @@ function BatchSettings({
           title={intl.formatMessage({
             id: 'components.BatchSettings.planTripTooltip'
           })}
+          type="submit"
         >
           <StyledIconWrapper style={{ fontSize: '1.6em' }}>
             {hasValidLocation(currentQuery, 'from') &&
