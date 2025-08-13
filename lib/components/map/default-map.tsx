0a0b3208f3616b8ecef3479927d6ee6b3df8066e
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-08-13 09:23:09 -0400

    refactor(map): Remove mapbox CSS references.

--- lib/components/map/default-map.tsx
+++ lib/components/map/default-map.tsx
@@ -55,8 +55,7 @@ const MapContainer = styled.div`
     box-sizing: unset;
   }
 
-  .maplibregl-popup-content,
-  .mapboxgl-popup-content {
+  .maplibregl-popup-content {
     border-radius: 10px;
     box-shadow: 0 3px 14px 4px rgb(0 0 0 / 20%);
   }
