610ed2d375a89cb079832e2a167f084f069f6492
Amy Corson <115499534+amy-corson-ibigroup@users.noreply.github.com>
2025-08-15 14:29:19 -0500

    re-order pattern filtering

--- lib/util/pattern-viewer.ts
+++ lib/util/pattern-viewer.ts
@@ -29,24 +29,14 @@ export function extractMainHeadsigns(
     const alreadyExistingIndex = prev.findIndex(
       (h) => h.headsign === cur.headsign
     )
-    // With all duplicate headsigns with the same last stops, only keep the pattern with the
-    // longest geometry.
-    if (
-      alreadyExistingIndex >= 0 &&
-      amended[alreadyExistingIndex].lastStop === cur.lastStop
-    ) {
-      if (amended[alreadyExistingIndex].geometryLength < cur.geometryLength) {
-        amended[alreadyExistingIndex] = cur
-        // If we're replacing the already existing index, no need to continue
-        return amended
-      }
-    } else {
-      amended.push(cur)
-    }
+
+    // If we encounter a duplicate headisgn, we may rename it in the case that the last stop is different than the headsign
     if (
       alreadyExistingIndex >= 0 &&
       cur.lastStop &&
-      cur.headsign !== cur.lastStop
+      cur.headsign !== cur.lastStop &&
+      // If the last stop is different than the headsign but the same as the last stop of the previously existing duplicate, there's no point in renaming.
+      cur.lastStop !== amended[alreadyExistingIndex].lastStop
     ) {
       editHeadsign(cur)
 
@@ -57,6 +47,20 @@ export function extractMainHeadsigns(
         amended.push(cur)
         return amended
       }
+    }
+
+    // With all duplicate headsigns with the same last stops, only keep the pattern with the
+    // longest geometry.
+    if (
+      alreadyExistingIndex >= 0 &&
+      amended[alreadyExistingIndex].lastStop === cur.lastStop
+    ) {
+      if (amended[alreadyExistingIndex].geometryLength < cur.geometryLength) {
+        amended[alreadyExistingIndex] = cur
+        // If we're replacing the already existing index, no need to continue
+      }
+    } else {
+      amended.push(cur)
     }
 
     return amended
