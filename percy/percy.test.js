b4e7af4614df66ade86767058abff8c9b0b40b21
miles-grant-ibigroup <miles.grant@ibigroup.com>
2026-01-07 23:31:12 +0100

    printable-itinerary updates fixes

--- percy/percy.test.js
+++ percy/percy.test.js
@@ -141,10 +141,10 @@ async function executeTest(page, isMobile, isCallTaker) {
     await page.keyboard.type('08072023') // MMDDYYYY format.
 
     // Open advanced settings and wait for animation
-    await page.waitForTimeout(1500)
     await page.click('#open-advanced-settings-button')
-    await page.waitForTimeout(1500)
+    await page.waitForTimeout(500)
 
+    await page.click('#transit')
     // Check submode selector (this will have no effect on mock query)
     await page.click('#id-query-param-tram')
 
@@ -385,8 +385,10 @@ async function executeTest(page, isMobile, isCallTaker) {
   if (!isCallTaker) {
     // Go to a URL that will trigger an error message.
     await page.goto(
-      `http://localhost:${MOCK_SERVER_PORT}/#/?ui_activeSearch=fg33svlbf&ui_activeItinerary=-1&fromPlace=TestLocation%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A34.78946214120528%2C-86.37663414886111&toPlace=1%20TestLocation2%20Avenue%20NE%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A35.767060728439574%2C-86.35749390533111&date=2023-08-09&time=17%3A56&arriveBy=false&mode=BICYCLE&walkSpeed=1.34&numItineraries=3&modeButtons=walk_bike_car_transit`
+      `http://localhost:${MOCK_SERVER_PORT}/#/?ui_activeSearch=fg33svlbf&ui_activeItinerary=-1&fromPlace=TestLocation%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A34.78946214120528%2C-86.37663414886111&toPlace=1%20TestLocation2%20Avenue%20NE%2C%20Atlanta%2C%20GA%2C%20USA%3A%3A35.767060728439574%2C-86.35749390533111&date=2023-08-09&time=17%3A56&arriveBy=false&mode=BICYCLE&walkSpeed=1.34&numItineraries=3&modeButtons=bike_car_transit`
     )
+    await page.reload()
+    await page.waitForTimeout(500)
     await openEditIfNeeded(page, isMobile)
     await page.click('#plan-trip')
     // FIXME: Network idle condition seems never met after navigating to above link.
