d53eda91523d86696062577cef8df002ab8a4783
binh-dam-ibigroup <binh.dam@arcadis.com>
2026-03-06 17:23:17 -0500

    refactor(TripNotificationPane): Convert to pure component

--- lib/components/user/monitored-trip/trip-notifications-pane.tsx
+++ lib/components/user/monitored-trip/trip-notifications-pane.tsx
@@ -3,7 +3,7 @@ import { ExclamationTriangle } from '@styled-icons/fa-solid/ExclamationTriangle'
 import { FormattedList, FormattedMessage } from 'react-intl'
 import { FormikProps } from 'formik'
 import { isTransitLeg } from '@opentripplanner/core-utils/lib/itinerary'
-import React, { Component, FormEvent } from 'react'
+import React, { FormEvent, useCallback } from 'react'
 import styled from 'styled-components'
 
 import {
@@ -52,148 +52,148 @@ interface Props extends FormikProps<MonitoredTrip> {
 /**
  * This component wraps the elements to edit trip notification settings.
  */
-class TripNotificationsPane extends Component<Props> {
-  _handleDelayThresholdChange = (e: FormEvent<FormControl>): void => {
-    // To spare users the complexity of the departure/arrival delay thresholds,
-    // set both the arrival and departure variance delays to the selected value.
-    const { setFieldValue } = this.props
-    const target = e.target as HTMLSelectElement
-    if (target) {
-      const threshold = target.value
-      setFieldValue('arrivalVarianceMinutesThreshold', threshold)
-      setFieldValue('departureVarianceMinutesThreshold', threshold)
-    }
-  }
+const TripNotificationsPane = (props: Props): JSX.Element => {
+  const { isReadOnly, notificationChannel, setFieldValue, values } = props
+  const handleDelayThresholdChange = useCallback(
+    (e: FormEvent<FormControl>): void => {
+      // To spare users the complexity of the departure/arrival delay thresholds,
+      // set both the arrival and departure variance delays to the selected value.
+      const target = e.target as HTMLSelectElement
+      if (target) {
+        const threshold = target.value
+        setFieldValue('arrivalVarianceMinutesThreshold', threshold)
+        setFieldValue('departureVarianceMinutesThreshold', threshold)
+      }
+    },
+    [setFieldValue]
+  )
 
-  render(): JSX.Element {
-    const { isReadOnly, notificationChannel, values } = this.props
-    const areNotificationsDisabled =
-      notificationChannel === 'none' || !notificationChannel?.length
-    // Define a common trip delay field for simplicity, set to the smallest between the
-    // retrieved departure/arrival delay attributes.
-    const commonDelayThreshold = Math.min(
-      values.arrivalVarianceMinutesThreshold,
-      values.departureVarianceMinutesThreshold
+  const areNotificationsDisabled =
+    notificationChannel === 'none' || !notificationChannel?.length
+  // Define a common trip delay field for simplicity, set to the smallest between the
+  // retrieved departure/arrival delay attributes.
+  const commonDelayThreshold = Math.min(
+    values.arrivalVarianceMinutesThreshold,
+    values.departureVarianceMinutesThreshold
+  )
+  const hasTransit = values.itinerary?.legs?.some(isTransitLeg)
+
+  let notificationSettingsContent
+  if (areNotificationsDisabled) {
+    notificationSettingsContent = (
+      <Alert bsStyle="warning">
+        <p>
+          <IconWithText Icon={ExclamationTriangle}>
+            <strong>
+              <FormattedMessage id="components.TripNotificationsPane.notificationsTurnedOff" />
+            </strong>
+          </IconWithText>
+        </p>
+        <p>
+          <FormattedMessage id="components.TripNotificationsPane.howToReceiveAlerts" />
+        </p>
+      </Alert>
     )
-    const hasTransit = values.itinerary?.legs?.some(isTransitLeg)
-
-    let notificationSettingsContent
-    if (areNotificationsDisabled) {
-      notificationSettingsContent = (
-        <Alert bsStyle="warning">
-          <p>
-            <IconWithText Icon={ExclamationTriangle}>
-              <strong>
-                <FormattedMessage id="components.TripNotificationsPane.notificationsTurnedOff" />
-              </strong>
-            </IconWithText>
-          </p>
-          <p>
-            <FormattedMessage id="components.TripNotificationsPane.howToReceiveAlerts" />
-          </p>
-        </Alert>
-      )
-    } else {
-      const selectedChannels = notificationChannel
-        .split(',')
-        .filter((channel) => channel?.length)
-        .map((channel) => (
-          <FormattedMessage
-            id={`common.notifications.${channel}`}
-            key={channel}
-          />
-        ))
-      notificationSettingsContent = (
-        <FieldSet disabled={isReadOnly}>
-          {hasTransit ? (
-            <>
-              <legend>
-                <FormattedMessage
-                  id="components.TripNotificationsPane.notifyViaChannelWhen"
-                  values={{
-                    channel: (
-                      <FormattedList
-                        type="conjunction"
-                        value={selectedChannels}
-                      />
-                    )
-                  }}
-                />
-              </legend>
-              <SettingsList>
-                <li>
-                  <Select
-                    label={
-                      <FormattedMessage id="components.TripNotificationsPane.realtimeAlertFlagged" />
-                    }
-                    name="notifyOnAlert"
-                  >
-                    <YesNoOptions defaultValue />
-                  </Select>
-                </li>
-                <li>
-                  <Select
-                    label={
-                      <FormattedMessage id="components.TripNotificationsPane.altRouteRecommended" />
-                    }
-                    name="notifyOnItineraryChange"
-                  >
-                    <YesNoOptions defaultValue />
-                  </Select>
-                </li>
-                <li>
-                  <label htmlFor="commonDelayThreshold">
-                    <FormattedMessage id="components.TripNotificationsPane.delaysAboveThreshold" />
-                  </label>
-                  <FormControl
-                    componentClass="select"
-                    id="commonDelayThreshold"
-                    // Special event handler, hence not using <Select> as above.
-                    onChange={this._handleDelayThresholdChange}
-                    value={commonDelayThreshold}
-                  >
-                    <DurationOptions
-                      defaultValue={5}
-                      minuteOptions={[5, 10, 15]}
+  } else {
+    const selectedChannels = notificationChannel
+      .split(',')
+      .filter((channel) => channel?.length)
+      .map((channel) => (
+        <FormattedMessage
+          id={`common.notifications.${channel}`}
+          key={channel}
+        />
+      ))
+    notificationSettingsContent = (
+      <FieldSet disabled={isReadOnly}>
+        {hasTransit ? (
+          <>
+            <legend>
+              <FormattedMessage
+                id="components.TripNotificationsPane.notifyViaChannelWhen"
+                values={{
+                  channel: (
+                    <FormattedList
+                      type="conjunction"
+                      value={selectedChannels}
                     />
-                  </FormControl>
-                </li>
-              </SettingsList>
-            </>
-          ) : null}
-
-          <details>
-            <Summary>
-              <FormattedMessage id="components.TripNotificationsPane.advancedSettings" />
-            </Summary>
+                  )
+                }}
+              />
+            </legend>
             <SettingsList>
               <li>
                 <Select
                   label={
-                    <FormattedMessage id="components.TripNotificationsPane.monitorThisTrip" />
+                    <FormattedMessage id="components.TripNotificationsPane.realtimeAlertFlagged" />
                   }
-                  name="leadTimeInMinutes"
+                  name="notifyOnAlert"
                 >
-                  <DurationOptions
-                    decoratorFunc={(time, intl) => {
-                      return intl.formatMessage(
-                        { id: 'components.TripNotificationsPane.timeBefore' },
-                        { time }
-                      )
-                    }}
-                    defaultValue={30}
-                    minuteOptions={[15, 30, 45, 60]}
-                  />
+                  <YesNoOptions defaultValue />
                 </Select>
               </li>
+              <li>
+                <Select
+                  label={
+                    <FormattedMessage id="components.TripNotificationsPane.altRouteRecommended" />
+                  }
+                  name="notifyOnItineraryChange"
+                >
+                  <YesNoOptions defaultValue />
+                </Select>
+              </li>
+              <li>
+                <label htmlFor="commonDelayThreshold">
+                  <FormattedMessage id="components.TripNotificationsPane.delaysAboveThreshold" />
+                </label>
+                <FormControl
+                  componentClass="select"
+                  id="commonDelayThreshold"
+                  // Special event handler, hence not using <Select> as above.
+                  onChange={handleDelayThresholdChange}
+                  value={commonDelayThreshold}
+                >
+                  <DurationOptions
+                    defaultValue={5}
+                    minuteOptions={[5, 10, 15]}
+                  />
+                </FormControl>
+              </li>
             </SettingsList>
-          </details>
-        </FieldSet>
-      )
-    }
+          </>
+        ) : null}
 
-    return <div>{notificationSettingsContent}</div>
+        <details>
+          <Summary>
+            <FormattedMessage id="components.TripNotificationsPane.advancedSettings" />
+          </Summary>
+          <SettingsList>
+            <li>
+              <Select
+                label={
+                  <FormattedMessage id="components.TripNotificationsPane.monitorThisTrip" />
+                }
+                name="leadTimeInMinutes"
+              >
+                <DurationOptions
+                  decoratorFunc={(time, intl) => {
+                    return intl.formatMessage(
+                      { id: 'components.TripNotificationsPane.timeBefore' },
+                      { time }
+                    )
+                  }}
+                  defaultValue={30}
+                  minuteOptions={[15, 30, 45, 60]}
+                />
+              </Select>
+            </li>
+          </SettingsList>
+        </details>
+      </FieldSet>
+    )
   }
+
+  return <div>{notificationSettingsContent}</div>
 }
 
 export default TripNotificationsPane
