89d6d42ba3dfadc2751609927e7f6c3cb99ece44
Alec Georgoff <alec.georgoff@gmail.com>
2026-09-10 15:32:18 -0700

    Use default map icon if no icon present in geojson

--- lib/components/map/connected-geojson-layer.tsx
+++ lib/components/map/connected-geojson-layer.tsx
@@ -9,6 +9,21 @@ import React, { useEffect, useState } from 'react'
 import * as mapActions from '../../actions/map'
 import { SetLocationHandler } from '../util/types'
 
+const DefaultMapIcon = (name: string, className?: string) => (
+  <svg
+    aria-labelledby="svg-title"
+    className={className}
+    fill="currentColor"
+    height="16"
+    viewBox="0 0 16 16"
+    width="16"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <title id="svg-title">{name}</title>
+    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
+  </svg>
+)
+
 type Props = {
   setLocation: SetLocationHandler
   url: string
@@ -67,11 +82,15 @@ const GeoJSONOverlay = (props: Props) => {
             popupProps={{ offset: 10 }}
             position={[geometry.coordinates[1], geometry.coordinates[0]]}
           >
-            <img
-              alt={properties.Name}
-              className={properties.className}
-              src={properties.icon}
-            />
+            {properties.icon ? (
+              <img
+                alt={properties.Name}
+                className={properties.className}
+                src={properties.icon}
+              />
+            ) : (
+              DefaultMapIcon(properties.Name, properties.className)
+            )}
           </MarkerWithPopup>
         )
       })}
