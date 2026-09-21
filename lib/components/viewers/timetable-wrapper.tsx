8053271999f6ba0a242857f7db61ac99074e379e
Alec Georgoff <alec.georgoff@gmail.com>
2026-09-21 09:49:06 -0700

    Implement notices in timetable and update query

--- lib/components/viewers/timetable-wrapper.tsx
+++ lib/components/viewers/timetable-wrapper.tsx
@@ -132,6 +132,7 @@ const TimeTableWrapper = (props: TimeTableWrapperProps): JSX.Element => {
             includeDwellStops
             route={routeInformation}
             showBlockId
+            showNotices
             timepointsOnly={timepointsOnly}
           />
         </div>
